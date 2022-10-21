de12a4f96946df77d9ab0b592435dfb483530cca
vishal1844 <104387858+vishal1844@users.noreply.github.com>
2022-10-21 17:47:35 +0530

    Time: 372 ms (50.98%), Space: 72.9 MB (83.64%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
-    // bool containsNearbyDuplicate(vector<int>& nums, int k) {
-    //     int i,j,t,l,s;
-    //     for(i=0;i<nums.size()-1;i++){
-    //         for(j=i+1;j<nums.size();j++){
-    //             if(nums[i]==nums[j]){
-    //                 if(abs(i-j)<=k){
-    //                 return true;}
-    //             }
-    //         }
-    //     }
-    //        return false;
-    // }
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-       unordered_map<int, int>m;
-        for(int i=0; i<nums.size(); i++){
-           // if(m.count(nums[i])){
-            if(m.find(nums[i])!=m.end()){
-                int diff=abs(m[nums[i]]-i);
-                //cout<<m[nums[i]]<<endl;
-                if(diff<=k) return true;
-                
-            }
-          m[nums[i]]=i;
+        vector<pair<int,int>>v;
+        int i,j,l,m,t;
+        for(i=0;i<nums.size();i++){
+            v.push_back({nums[i],i});
         }
-      return false;
+        sort(v.begin(),v.end());
+        for(i=0;i<v.size()-1;i++){
+            if((v[i].first==v[i+1].first)&&((abs(v[i].second-v[i+1].second))<=k))
+                return true;
+        }
+        return false;
     }
 };
