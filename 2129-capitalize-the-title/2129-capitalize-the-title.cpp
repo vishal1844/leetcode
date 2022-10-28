15b2223a49ad6f2b838a1294f51c61f6cf2f2346
vishal1844 <104387858+vishal1844@users.noreply.github.com>
2022-10-28 18:01:02 +0530

    Time: 3 ms (71.45%), Space: 6.2 MB (72.73%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.cpp
+++ 2129-capitalize-the-title/2129-capitalize-the-title.cpp
@@ -3,31 +3,19 @@ public:
     string capitalizeTitle(string title) {
         int i,j,k,l,m,t=0;
         string s;
-        //cout<<title.size();
         for(i=0;i<title.size();i++){
             title[i]=tolower(title[i]);
         }
-       // s[0]=toupper(s[0]);
         for(i=0;i<title.size();i++){
             t=0;
-            // if(s[i]!=' '){
-            //     l=i;
-                //cout<<i;
             l=i;
-            j=i;
-                while(j<title.size()&&title[j]!=' '){
-                    //cout<<i<<" ";
-                    
+                while(i<title.size()&&title[i]!=' '){
                     t++;
-                    j++;
+                    i++;
                 }
-            //cout<<t<<" ";
-            i=j;
             if(t>2){
-                //cout<<"hello";
                 title[l]=toupper(title[l]);
             }
-            //cout<<i;
         }
         return title;
     }
