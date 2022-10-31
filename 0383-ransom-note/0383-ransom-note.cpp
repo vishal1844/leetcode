46cad2e8591f3666570c1297377642640f9398cd
vishal1844 <104387858+vishal1844@users.noreply.github.com>
2022-11-01 00:26:19 +0530

    Time: 58 ms (23.88%), Space: 8.8 MB (39.74%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -7,17 +7,12 @@ public:
             mp[magazine[i]]++;
         }
         for(i=0;i<ransomNote.size();i++){
-            //for(auto itr:mp){
             if(mp.find(ransomNote[i])!=mp.end()&&mp[ransomNote[i]]!=0){
                 --(mp[ransomNote[i]]);
-                //cout<<"helo";
-                //cout<<mp[ransomNote[i]];
-                //(itr.second)--;
             }
             else
                 return false;
             }
-        //}
         return true;
     }
 };
