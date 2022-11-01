2f3122106fd5e8d8ddb236d5a2870b85dce69e2d
vishal1844 <104387858+vishal1844@users.noreply.github.com>
2022-11-01 22:49:39 +0530

    Time: 0 ms (100.00%), Space: 6 MB (68.95%) - LeetHub

--- 0917-reverse-only-letters/0917-reverse-only-letters.cpp
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     string reverseOnlyLetters(string s) {
-        int i,j,k,l,t;
+        int i,j,t;
         for(i=0,j=s.size()-1;i<=j;){
             if(((s[i]>='A'&&s[i]<='Z')||(s[i]>='a'&&s[i]<='z'))&&((s[j]>='A'&&s[j]<='Z')||(s[j]>='a'&&s[j]<='z'))){
                 char c=s[i];
                 s[i]=s[j];
                 s[j]=c;
-                //cout<<s[i]<<s[j];
                 i++;
                 j--;
             }
-            else if(!((s[i]>='A'&&s[i]<='Z')||(s[i]>='a'&&s[i]<='z'))){
+            else if(!((s[i]>='A'&&s[i]<='Z')||(s[i]>='a'&&s[i]<='z')))
                 i++;
-                //cout<<i<<" ";
-            }
-            else if(!((s[j]>='A'&&s[j]<='Z')||(s[j]>='a'&&s[j]<='z'))){
+            else if(!((s[j]>='A'&&s[j]<='Z')||(s[j]>='a'&&s[j]<='z')))
                 j--;
-            //cout<<j<<" ";
-            }
         }
         return s;
     }
