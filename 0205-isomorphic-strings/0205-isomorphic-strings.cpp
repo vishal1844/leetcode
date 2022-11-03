104536523b92384bef0fcb20680da15d11b77d19
vishal1844 <104387858+vishal1844@users.noreply.github.com>
2022-11-03 23:16:05 +0530

    Time: 10 ms (73.98%), Space: 7 MB (45.72%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -4,10 +4,12 @@ public:
         int i,j;
         unordered_map<char,char>mp,m;
         for(i=0;i<s.size();i++){
-             if(mp.find(s[i])!=mp.end()||m.find(t[i])!=m.end()){
-                if(mp[s[i]]!=t[i]||m[t[i]]!=s[i])
-                    return 0;
-            }
+            //  if(mp.find(s[i])!=mp.end()||m.find(t[i])!=m.end()){
+            //     if(mp[s[i]]!=t[i]||m[t[i]]!=s[i])
+            //         return 0;
+            // }
+            if(mp[s[i]]&&mp[s[i]]!=t[i]) return 0;
+            if(m[t[i]]&&m[t[i]]!=s[i]) return 0;
             mp[s[i]]=t[i];
                    m[t[i]]=s[i];
         }
