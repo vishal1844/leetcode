c803fc20c0e5743fd82e42333cff2c95319ba835
vishal1844 <104387858+vishal1844@users.noreply.github.com>
2022-11-04 23:48:34 +0530

    Time: 4 ms (95.97%), Space: 7.6 MB (85.11%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -5,11 +5,9 @@ public:
         l=0;
         k=s.size()-1;
         while(l<k){
-            //cout<<"jgf";
             if((s[l]=='a'||s[l]=='e'||s[l]=='i'||s[l]=='o'||s[l]=='u'||s[l]=='A'||s[l]=='E'||s[l]=='I'||s[l]=='O'||s[l]=='U')&&(s[k]=='a'||s[k]=='e'||s[k]=='i'||s[k]=='o'||s[k]=='u'||s[k]=='A'||s[k]=='E'||s[k]=='I'||s[k]=='O'||s[k]=='U')){
                 swap(s[l],s[k]);
                 l++;
-                //cout<<"uruji";
                 k--;
         }
             else if((s[l]=='a'||s[l]=='e'||s[l]=='i'||s[l]=='o'||s[l]=='u'||s[l]=='A'||s[l]=='E'||s[l]=='I'||s[l]=='O'||s[l]=='U')&&(s[k]!='a'||s[k]!='e'||s[k]!='i'||s[k]!='o'||s[k]!='u'||s[k]!='A'||s[k]!='E'||s[k]!='I'||s[k]!='O'||s[k]!='U')){
