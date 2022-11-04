874ff03818b4da62599313afdc6ad8e83f3c3914
vishal1844 <104387858+vishal1844@users.noreply.github.com>
2022-11-05 01:59:02 +0530

    Time: 10 ms (23.69%), Space: 6.4 MB (37.01%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int l=a.size();
-        int h=b.size();
+        int l=a.size()-1;
+        int h=b.size()-1;
         int i,c=0,j,k;
         string s="";
-        while(l>0||h>0||c==1){
-            if(l>0) c+=(a[--l]-'0');
-            if(h>0) c+=(b[--h]-'0');
+        while(l>=0||h>=0||c==1){
+            if(l>=0) c+=(a[l--]-'0');
+            if(h>=0) c+=(b[h--]-'0');
             s+=char(c%2+'0');
             c=c/2;
         }
