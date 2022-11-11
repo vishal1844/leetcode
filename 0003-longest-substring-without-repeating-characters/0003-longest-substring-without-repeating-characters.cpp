46176cb3d3f07beda7d1720eca0572b6a3ea7173
vishal1844 <104387858+vishal1844@users.noreply.github.com>
2022-11-11 16:03:42 +0530

    Time: 1724 ms (5.01%), Space: 149.6 MB (6.81%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -7,16 +7,12 @@ public:
         for(i=0;i<s.size();i++){
             for(j=i;j<s.size();j++){
             if(st.find(s[j])!=st.end()){
-                if(t<st.size()){
-                    //cout<<s[j]<<" "<<st.size()<<t;
+                if(t<st.size())
                     t=st.size();
-                }
-                //t=max(t,st.size());
                 st.clear();
                 break;
             }
             st.insert(s[j]);
-                //cout<<"hello";
         }
         }
         return t;
