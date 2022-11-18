6110a9976c74c896a69fa42058608482f1383d51
vishal1844 <104387858+vishal1844@users.noreply.github.com>
2022-11-18 16:18:08 +0530

    Time: 15 ms (57.40%), Space: 5.9 MB (75.31%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.cpp
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -6,12 +6,13 @@ public:
         int t=(ax2-ax1)*(ay2-ay1);
         int k=(bx2-bx1)*(by2-by1);
         int s=t+k; 
-        if (ax2 <= bx1 || ay1 >= by2 || ax1 >= bx2 || ay2<= by1) {
-        return s;
-        }
-        int a,b,l,m;
+        // if (ax2 <= bx1 || ay1 >= by2 || ax1 >= bx2 || ay2<= by1) {
+        // return s;
+        // }
+        int a,b=0,l,m;
         l=min(ax2,bx2)-max(ax1,bx1);
         m=min(ay2,by2)-max(ay1,by1);
+        if(l>0&&m>0)
         b=l*m;
         return s-b; 
     }
