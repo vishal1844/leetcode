4c9522347a864c677008d7160d43bca51a685f6c
vishal1844 <104387858+vishal1844@users.noreply.github.com>
2022-11-28 20:27:48 +0530

    Time: 1094 ms (73.28%), Space: 182.3 MB (39.91%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,42 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        unordered_set<int>st;
-        unordered_map<int,int>mp;
-        int i,j,k,l,m,t;
-        vector<vector<int>>v;
-        vector<int>vec,veci;
-        for(i=0;i<matches.size();i++){
-            st.insert(matches[i][0]);
-        }
-        for(i=0;i<matches.size();i++){
-            mp[matches[i][1]]++;
+        int i,j,k,l,t;
+        map<int,int>mp;
+        vector<int>v,vec;
+        for(auto itr:matches){
+            if(!mp.count(itr[0])) mp[itr[0]]=0;
+            mp[itr[1]]++;
         }
         for(auto itr:mp){
-            if(st.find(itr.first)!=st.end()){
-                // if(itr.second>=2){
-                //     st.erase(itr.first);
-                // }
-                // else{
-                //     vec.push_back(itr.first);
-                //     st.erase(itr.first);
-                // }
-                if(itr.second==1){
-                    vec.push_back(itr.first);
-                }
-                st.erase(itr.first);
+            if(itr.second==0){
+                v.push_back(itr.first);
             }
-            else if(itr.second==1){
+            if(itr.second==1){
                 vec.push_back(itr.first);
             }
         }
-        sort(vec.begin(),vec.end());
-        for(auto itr:st){
-            veci.push_back(itr);
-        }
-        sort(veci.begin(),veci.end());
-        v.push_back(veci);
-        v.push_back(vec);
-        return v;
+        return{v,vec};
     }
 };
