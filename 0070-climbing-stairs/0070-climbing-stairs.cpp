d625cb105eeef22dfde1cf63756c580f2b593516
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2022-12-13 07:58:29 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (83.29%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    int dp[46];
+public:
+int solve(int n){
+    if(n==0)return 1;
+    if(n<=0)return 0;
+    if(dp[n]!=-1){
+        return dp[n];
+    }
+    return dp[n]=solve(n-1)+solve(n-2);
+}
+    int climbStairs(int n) {
+        if(n==0||n==1)return n;
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
+    }
+};
