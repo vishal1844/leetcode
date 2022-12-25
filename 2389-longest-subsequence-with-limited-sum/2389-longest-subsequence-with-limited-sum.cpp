5e73a1e8d1c3a3f8f5d9d5053588a35fac997fe6
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2022-12-25 17:17:48 +0530

    Time: 15 ms (96.60%), Space: 13.7 MB (83.30%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
-    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+    vector<int> answerQueries(vector<int>& nums, vector<int>& q) {
+        int n=nums.size();
+        int m=q.size();
+        vector<int>ans(m);
         sort(nums.begin(),nums.end());
-        vector<int>v;
-        int i,k,l,m=0,t=0,j=0;
-        unordered_map<int,int>mp;
-        vector<int>vec=queries;
-        sort(queries.begin(),queries.end());
-        for(i=0;i<queries.size();i++){
-            while(t<queries[i]&&j<nums.size()){
-                t+=nums[j];
-                j++;
-                m++;
-            }
-            if(t<=queries[i]){
-                mp[queries[i]]=m;
-            }
-            else{
-                mp[queries[i]]=m-1;
-            }
+        for(int i=1;i<n;++i){
+            nums[i]+=nums[i-1];
         }
-        for(i=0;i<vec.size();i++){
-            v.push_back(mp[vec[i]]);
+        for(int i=0;i<m;++i){
+            ans[i]=upper_bound(nums.begin(),nums.end(),q[i])-nums.begin();
         }
-        return v;
+        return ans;
     }
 };
