121db7ffaec60b56446459224f64b3d6418f44bc
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-01-15 01:48:07 +0530

    Time: 3 ms (79.82%), Space: 6.5 MB (61.47%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
@@ -1,59 +1,30 @@
 class Solution {
 public:
-    void DFS(int src, array<array<int, 26>, 26>& adjMatrix, array<int, 26>& visited, vector<int>& component, int& minChar) {
-        // Mark the character as visited.
-        visited[src] = 1;
-        // Add it to the list.
-        component.push_back(src);
-        // Update the minimum character in the component.
-        minChar = min(minChar, src);
-        
-        for (int i = 0; i < 26; i++) {
-            // Perform DFS, if the edge exists and the node isn't visited yet.
-            if (adjMatrix[src][i] && !visited[i]) {
-                DFS(i, adjMatrix, visited, component, minChar);
-            }
-        }
+    int par[26];
+    
+    int find(int x){
+        if(par[x]==-1) return x;
+        return par[x]=find(par[x]);
     }
     
+    void Union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        
+        if (x != y) 
+            par[max(x, y)] = min(x, y); 
+    }
+	
     string smallestEquivalentString(string s1, string s2, string baseStr) {
-        // Adjacency matrix to store edges.
-        array<array<int, 26>, 26> adjMatrix = {0};
-        for (int i = 0; i < s1.size(); i++) {
-            adjMatrix[s1[i] - 'a'][s2[i] - 'a'] = 1;
-            adjMatrix[s2[i] - 'a'][s1[i] - 'a'] = 1;
-        }
         
-        // Array to store the final character mappings.
-        array<int, 26> mappingChar = {0};
-        for (int i = 0; i < 26; i++) {
-            mappingChar[i] = i;
-        }
+        memset(par, -1, sizeof(par));
         
-        // Array to keep visited nodes during DFS.
-        array<int, 26> visited = {0};
-        for (int c = 0; c < 26; c++) {
-            if (!visited[c]) {
-                // Store the characters in the current component.
-                vector<int> component;
-                // Variable to store the minimum character in the component.
-                int minChar = 27;
+        for (auto i = 0; i < s1.size(); ++i) 
+            Union(s1[i] - 'a', s2[i] - 'a');
+        
+        for(auto i=0;i<baseStr.size();i++) 
+            baseStr[i]=find(baseStr[i]-'a')+'a';
 
-                DFS(c, adjMatrix, visited, component, minChar);
- 
-                // Map the characters in the component to the minimum character.
-                for (int vertex : component) {
-                    mappingChar[vertex] = minChar;
-                }
-            }
-        }
-        
-        string ans;
-        // Create the answer string.
-        for (char c : baseStr) {
-            ans += (char)(mappingChar[c - 'a'] + 'a');
-        }
-        
-        return ans;
+        return baseStr;
     }
 };
