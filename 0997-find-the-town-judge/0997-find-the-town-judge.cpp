24dcc0e248386114832ce7b2356c8c3a613a1692
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-01-23 16:52:56 +0530

    Time: 416 ms (21.03%), Space: 60.7 MB (95.56%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
         int i,j,k,l,m,t=0;
-        vector<pair<int,int>>v(n+1,{0,0});
+        vector<int>v(n+1,0);
+        vector<int>vec(n+1,0);
         for(i=0;i<trust.size();i++){
-           v[trust[i][0]].first++;
-            v[trust[i][1]].second++;
+            v[trust[i][0]]++;
+            vec[trust[i][1]]++;
         }
         for(i=1;i<=n;i++){
-            if(v[i].first==0&&v[i].second==n-1)
-                return i;
+            if(v[i]==0&&vec[i]==n-1)return i;
         }
         return -1;
     }
