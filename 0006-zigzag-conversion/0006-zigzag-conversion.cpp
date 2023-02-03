591c5c63b7b8fd4c0e838755caa2eeda3d63baf0
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-02-04 03:46:56 +0530

    Time: 60 ms (20.98%), Space: 33.7 MB (14.22%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,21 +1,49 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        int i,j,k,l,m,t=0;
-        if(numRows<=1){
+        if (numRows == 1) {
             return s;
         }
-        string st="";
-        for(i=0;i<numRows;i++){
-            for(j=i;j<s.size();){
-                st+=s[j];
-                if(i>0&&i<numRows-1&&(((j+(numRows-1)*2)-(2*i))<s.size())){
-                    st+=s[((j+(numRows-1)*2)-(2*i))];
-                }
-                j+=((numRows-1)*2);
+        
+        int n = int(s.size());
+        int sections = ceil(n / (2 * numRows - 2.0));
+        int numCols = sections * (numRows - 1);
+        
+        vector<vector<char>> matrix(numRows, vector<char>(numCols, ' '));
+        
+        int currRow = 0, currCol = 0;
+        int currStringIndex = 0;
+        
+        // Iterate in zig-zag pattern on matrix and fill it with string characters.
+        while (currStringIndex < n) {
+            // Move down.
+            while (currRow < numRows && currStringIndex < n) {
+                matrix[currRow][currCol] = s[currStringIndex];
+                currRow++;
+                currStringIndex++;
+            }
+            
+            currRow -= 2;
+            currCol++;
+            
+            // Move up (with moving right also).
+            while (currRow > 0 && currCol < numCols && currStringIndex < n) {
+                matrix[currRow][currCol] = s[currStringIndex];
+                currRow--;
+                currCol++;
+                currStringIndex++;
             }
         }
-        return st;
         
+        string answer;
+        for (auto& row: matrix) {
+            for (auto& character: row) {
+                if (character != ' ') {
+                    answer += character;
+                }
+            }
+        }
+        
+        return answer;
     }
 };
