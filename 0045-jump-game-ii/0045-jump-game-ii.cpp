cfb67bc1c2462d0204c8d6353dc7433828dfa774
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-02-08 11:09:46 +0530

    Time: 19 ms (69.69%), Space: 16.7 MB (56.17%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int i,j,k,l=0,m=0,t=0;
+        int n=nums.size()-1;
+        k=n;
+        for(i=0;i<n;i++){
+           m=max(m,nums[i]+i);
+            if(l==i){
+                l=m;
+                t++;
+                if(l>=k){
+                    return t; 
+                }
+            }
+        }
+        return t;
+    }
+};
