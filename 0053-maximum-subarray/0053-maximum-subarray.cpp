0a466fe1ae4af8eeca49a5706ec92b2773b7c60d
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-03-18 12:43:37 +0530

    Time: 379 ms (5.07%), Space: 67.9 MB (18.16%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,12 +3,24 @@ public:
     int maxSubArray(vector<int>& nums) {
         int i,j,k,l,m,t=0,maxi=INT_MIN;
         for(i=0;i<nums.size();i++){
+            if(t==0){
+                l=i;
+            }
             t+=nums[i];
-            maxi=max(maxi,t);
+            if(t>maxi){
+                k=l;
+                m=i;
+                maxi=t;
+            }
+            //maxi=max(maxi,t);
             if(t<0){
                 t=0;
             }
         }
+        for(i=k;i<=m;i++){
+            cout<<nums[i]<<" ";
+        }
+        cout<<endl;
         return maxi;
     }
 };
