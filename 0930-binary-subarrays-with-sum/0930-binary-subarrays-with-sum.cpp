db9185da39fc71004395f772a865f6080605d71d
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-03-31 00:27:03 +0530

    Time: 102 ms (8.56%), Space: 37.9 MB (5.01%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int i,j,k,l,m=0,t=0;
+        map<int,int>mp;
+        mp[0]=1;
+        for(i=0;i<nums.size();i++){
+            t+=nums[i];
+            m+=mp[t-goal];
+            mp[t]++;
+        }
+        return m;
+        
+    }
+};
