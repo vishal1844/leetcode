e966fc9b573da50cfd108ac85f1be3e3f889efef
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-04-06 00:17:11 +0530

    Time: 21 ms (75.08%), Space: 16.4 MB (64.55%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,28 +1,35 @@
 class MinStack {
-    stack<int>s1,s2;
+    stack<int>st;
+    int mini=INT_MAX;
 public:
     MinStack() {
+        
     }
     
     void push(int val) {
-        if(s2.empty()||(val<=s2.top())){
-            s2.push(val);
-        }
-        s1.push(val);
+        if(val<=mini){
+                st.push(mini);
+                mini=val;
+            }
+            st.push(val);
     }
     
     void pop() {
-        if(s1.top()==s2.top())
-            s2.pop();
-        s1.pop();
+        if(st.top()==mini){
+            st.pop();
+            mini=st.top();
+            st.pop();
+        }else{
+            st.pop();
+        }
     }
     
     int top() {
-        return s1.top();
+        return st.top();
     }
     
     int getMin() {
-        return s2.top();
+        return mini;
     }
 };
 
