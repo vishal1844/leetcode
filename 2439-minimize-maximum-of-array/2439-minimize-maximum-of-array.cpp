2286bd9985281f75b6f99e4f21aff236a4f7264a
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-04-06 00:33:22 +0530

    Time: 113 ms (97.99%), Space: 71.4 MB (61.54%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -4,8 +4,10 @@ public:
         long long int i,j,k=-1,l,s=0;
         for(i=0;i<nums.size();i++){
             s+=nums[i];
-            k=max(k,(s+i)/(i+1));
-        }
+           // k=max(k,(s+i)/(i+1));                  //why this becz 7=3+2+2 and maximum is 3
+            l=ceil((1.0*s)/(i+1));
+            k=max(k,l);
+        }                                          //so we take ceil also
         return k;
     }
 };
