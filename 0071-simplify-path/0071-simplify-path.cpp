16aed8b1834abb49ddef39e58eecfcd41ad5b3ee
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-04-13 00:22:33 +0530

    Time: 4 ms (78.99%), Space: 10.7 MB (31.86%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,46 +1,90 @@
+// class Solution {
+// public:
+//     string simplifyPath(string path) {
+//         int n=path.length();
+//         stack<string>s;
+                
+//         if(path[n-1]!='/') // so that initially path always ends with '/'
+//             path+="/", ++n;
+            
+//         int i=1; // since path always starts from '/'
+//         string ans="";
+//         string temp="";
+//         while(i<n){
+            
+//             if(path[i]=='/'){ // check only if we encounter '/'
+                
+//                 if(temp=="" || temp=="."){
+//                     // ignore
+//                 }
+//                 else if(temp==".."){
+//                     if(!s.empty()) s.pop(); // pop the top element from stack if exists
+//                 }
+//                 else{
+//                     s.push(temp); //push the directory or file name to stack
+//                 }
+                
+//                 temp=""; // reset temp
+//             }
+//             else{
+//                 temp.push_back(path[i]); // else append to temp
+//             }
+            
+//             ++i; // increment index
+//         }
+        
+//         while(!s.empty()){ // add all the stack elements
+//             ans="/"+s.top()+ans;
+//             s.pop();
+//         }
+        
+//         if(ans.length()==0) // if no directory or file is present
+//             ans="/"; // minimum root directory must be present in ans
+        
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     string simplifyPath(string path) {
-        int n=path.length();
-        stack<string>s;
-                
-        if(path[n-1]!='/') // so that initially path always ends with '/'
-            path+="/", ++n;
-            
-        int i=1; // since path always starts from '/'
-        string ans="";
-        string temp="";
-        while(i<n){
-            
-            if(path[i]=='/'){ // check only if we encounter '/'
-                
-                if(temp=="" || temp=="."){
-                    // ignore
+        string s;
+        int i,j,k,l,m,t=0;
+        stack<string>st;
+        int n=path.size();
+        if(path[n-1]!='/'){
+            path+='/';
+            n++;
+        }
+        i=1;
+        while(i<path.size()){
+            if(path[i]=='/'){
+                if(s==""||s=="."){
+                    //ignore
                 }
-                else if(temp==".."){
-                    if(!s.empty()) s.pop(); // pop the top element from stack if exists
+                else if(s==".."){
+                    if(!st.empty()){
+                        st.pop();
+                    }
                 }
                 else{
-                    s.push(temp); //push the directory or file name to stack
+                    st.push(s);
                 }
-                
-                temp=""; // reset temp
+                s="";
             }
             else{
-                temp.push_back(path[i]); // else append to temp
+                s+=path[i];
             }
-            
-            ++i; // increment index
+            i++;
         }
-        
-        while(!s.empty()){ // add all the stack elements
-            ans="/"+s.top()+ans;
-            s.pop();
+        s="";
+       // string s;
+        while(!st.empty()){
+            s="/"+st.top()+s;
+            st.pop();
         }
-        
-        if(ans.length()==0) // if no directory or file is present
-            ans="/"; // minimum root directory must be present in ans
-        
-        return ans;
+        if(s.size()==0){
+            return "/";
+        }
+        return s;
     }
 };
