3c3c006b7f29579cc9e370013c38ac3102d845f3
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-04-19 19:46:17 +0530

    Time: 12 ms (92.10%), Space: 20.1 MB (90.75%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,45 +1,47 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int i,j,k,l,m,t=0;
         if(n==0){
             return true;
         }
-        if(flowerbed.size()==1&&n==1&&flowerbed[0]==0){
+        if(flowerbed.size()==1&&n==1&&flowerbed[flowerbed.size()-1]==0){
             return true;
         }
-        int i,j,k,l,m,t=0;
         for(i=0;i<flowerbed.size();i+=2){
             if(i==0){
                 if(flowerbed[i]==0&&flowerbed[i+1]==0){
-                flowerbed[i]=1;
-                t++;
+                    t++;
+                    //cout<<t<<" ";
+                    flowerbed[i]=1;
                 }
             }
+            // else if(i==flowerbed.size()-2&&flowerbed[i]==0&&flowerbed[i+1]==0){
+            //     t++;
+            //     //cout<<t<<" ";
+            //     flowerbed[i+1]=1;
+            // }
             else if(i==flowerbed.size()-1&&flowerbed[i-1]==0&&flowerbed[i]==0){
+                t++;
+                 //cout<<t<<" ";
                 flowerbed[i]=1;
-                t++;
             }
-            else if(i==flowerbed.size()-2&&flowerbed[i]==0&&flowerbed[i+1]==0){
-                flowerbed[i+1]=1;
+             else if(i==flowerbed.size()-2&&flowerbed[i]==0&&flowerbed[i+1]==0){
                 t++;
+                //cout<<t<<" ";
+                flowerbed[i+1]=1;
             }
             else if(flowerbed[i-1]==0&&flowerbed[i]==0&&flowerbed[i+1]==0){
-                flowerbed[i]=1;
                 t++;
+                //cout<<t<<" ";
+                flowerbed[i]=1;
             }
             if(t==n){
+                //ut<<t<<endl;
                 return true;
             }
         }
-        // if(flowerbed.size()%2==0){
-        //     if(flowerbed[i-2]==0&&flowerbed[i-1]==0){
-        //         t++;
-        //     }
-        // }
-        if(t==n){
-            return true;
-        }
+        //if(t==n)return true;
         return false;
-        
     }
 };
