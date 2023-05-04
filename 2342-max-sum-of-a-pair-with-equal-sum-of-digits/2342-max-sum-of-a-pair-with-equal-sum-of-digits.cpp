894b45e862e9aa74302cf5f181db519cf7805d0c
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-05-04 22:30:05 +0530

    Time: 644 ms (5.16%), Space: 60.1 MB (86.45%) - LeetHub

---
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int solve(int n){
+        int r,t=0;
+        while(n!=0){
+            r=n%10;
+            t+=r;
+            n/=10;
+        }
+        return t;
+    }
+public:
+    int maximumSum(vector<int>& nums) {
+        int i,j,k,l,m=0,t=-1;
+        map<int,int>mp;
+        for(i=0;i<nums.size();i++){
+            l=solve(nums[i]);
+            cout<<l<<" ";
+            if(mp.find(l)!=mp.end()){
+                t=max(t,mp[l]+nums[i]);
+                if(mp[l]<nums[i])mp[l]=nums[i];
+            }
+            else {
+                mp[l]+=nums[i];
+            }
+        }
+        return t;
+    }
+};
