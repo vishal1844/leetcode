be4980c70191cd2c7c4d8d2c8e6918e927645758
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-05-12 18:12:08 +0530

    Time: 596 ms (60.53%), Space: 8 MB (83.43%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,47 +1,35 @@
 class Solution {
-    bool solve(int i,int j,int w,int n,int m,vector<vector<char>>&board,string&word,vector<vector<char>>&visit){
-        if(w==word.size()){
+    int a[4]={-1,0,+1,0};
+    int b[4]={0,-1,0,+1};
+    bool solve(int i,int j,int n,int m,int w,vector<vector<char>>&board,string word,vector<vector<char>>&visit){
+        if(w==word.size()-1){
             return true;
         }
-        if(i<0||j<0||i>=n||j>=m){
-            return 0;
-        }
-        // if(w==word.size()){
-        //     return true;
-        // }
-        if(board[i][j]==word[w]&&visit[i][j]=='0'){
-            visit[i][j]='1';
-            if(solve(i+1,j,w+1,n,m,board,word,visit)){
-                return true;
+       // if(i<0||j<0||i>=n||j>=m)return 0;
+        for(int k=0;k<4;k++){
+            int nexti=i+a[k];
+            int nextj=j+b[k];
+            if(nexti>=0&&nextj>=0&&nexti<n&&nextj<m&&visit[nexti][nextj]!='1'&&board[nexti][nextj]==word[w+1]){
+                visit[nexti][nextj]='1';
+            if(solve(nexti,nextj,n,m,w+1,board,word,visit))return true;
+                visit[nexti][nextj]='0';
             }
-            visit[i][j]='0';
         }
-        if(board[i][j]==word[w]&&visit[i][j]=='0'){
-            visit[i][j]='1';
-            if(solve(i,j+1,w+1,n,m,board,word,visit))return true;
-            visit[i][j]='0';
-        }
-        if(board[i][j]==word[w]&&visit[i][j]=='0'){
-            visit[i][j]='1';
-            if(solve(i,j-1,w+1,n,m,board,word,visit))return true;
-            visit[i][j]='0';
-        }
-        if(board[i][j]==word[w]&&visit[i][j]=='0'){
-            visit[i][j]='1';
-            if(solve(i-1,j,w+1,n,m,board,word,visit))return true;
-            visit[i][j]='0';
-        }
-        return false;
+        return 0;
     }
 public:
     bool exist(vector<vector<char>>& board, string word) {
         vector<vector<char>>visit(board.size()+1,vector<char>(board[0].size()+1,'0'));
+        int a[4]={-1,0,+1,0};
+        int b[4]={0,-1,0,+1};
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
                 if(board[i][j]==word[0]){
-                    if(solve(i,j,0,board.size(),board[0].size(),board,word,visit)){
+                    visit[i][j]='1';
+                    if(solve(i,j,board.size(),board[0].size(),0,board,word,visit)){
                         return true;
                     }
+                    visit[i][j]='0';
                 }
             }
         }
