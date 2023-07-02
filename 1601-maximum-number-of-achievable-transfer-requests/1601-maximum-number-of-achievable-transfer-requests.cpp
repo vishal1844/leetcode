bca38a83bd7a7d311a93b215712bb7f96bb66660
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-07-03 02:32:12 +0530

    Time: 110 ms (68.52%), Space: 8.8 MB (75.93%) - LeetHub

--- 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
@@ -1,37 +1,28 @@
 class Solution {
-public:
-    int answer = 0;
-
-    void maxRequest(vector<vector<int>>& requests, vector<int>& indegree, int n, int index, int count) {
-        if (index == requests.size()) {
-            // Check if all buildings have an in-degree of 0.
-            for (int i = 0; i < n; i++) {
-                if (indegree[i]) {
-                    return;
-                }
+    vector<int>v;
+    int c=0;
+    void solve(int i,int m,int&t,int n,vector<vector<int>>&requests){
+        if(i==requests.size()){
+            for(auto itr:v){
+                if(itr)return;
             }
-            
-            answer = max(answer, count);
+            t=max(t,m);
             return;
         }
-        
-        // Consider this request, increment and decrement for the buildings involved.
-        indegree[requests[index][0]]--;
-        indegree[requests[index][1]]++;
-        // Move on to the next request and also increment the count of requests.
-        maxRequest(requests, indegree, n, index + 1, count + 1);
-        // Backtrack to the previous values to move back to the original state before the second recursion.
-        indegree[requests[index][0]]++;
-        indegree[requests[index][1]]--;
-        
-        // Ignore this request and move on to the next request without incrementing the count.
-        maxRequest(requests, indegree, n, index + 1, count);
+        v[requests[i][0]]--;
+        v[requests[i][1]]++;
+        solve(i+1,m+1,t,n,requests);
+        v[requests[i][0]]++;
+        v[requests[i][1]]--;
+        solve(i+1,m,t,n,requests);
     }
-    
+public:
     int maximumRequests(int n, vector<vector<int>>& requests) {
-        vector<int> indegree(n, 0);
-        maxRequest(requests, indegree, n, 0, 0);
-        
-        return answer;
+        int m=requests.size();
+        vector<int>vec(n,0);
+        v=vec;
+        int t=0;
+        solve(0,0,t,n,requests);
+        return t;
     }
 };
