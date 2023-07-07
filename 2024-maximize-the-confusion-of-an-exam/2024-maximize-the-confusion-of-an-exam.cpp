cadf37d6d8572f3f8a34e7a03a6fcc0d7e3bf252
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-07-08 00:34:27 +0530

    Time: 33 ms (41.69%), Space: 10 MB (95.61%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int maxConsecutiveAnswers(string answer, int k) {
+        int i,j=0,l,m=0,t=0;
+        for(i=0;i<answer.size();i++){
+            if(answer[i]=='T')t++;
+            else{
+                m++;
+            }
+        }
+        m=min(m,t);
+        if(m<=k){
+            return answer.size();
+        }
+        t=0;
+        int count=0;
+        for(i=0;i<answer.size();i++){
+            if(answer[i]=='F'){
+                count++;
+            }
+            if(count>k){
+                while(j<answer.size()&&answer[j]!='F'){
+                    j++;
+                }
+                j++;
+                count--;
+            }
+            t=max(t,i-j+1);
+        }
+         count=0;
+        j=0;
+        for(i=0;i<answer.size();i++){
+            if(answer[i]=='T'){
+                count++;
+            }
+            if(count>k){
+                while(j<answer.size()&&answer[j]!='T'){
+                    j++;
+                }
+                j++;
+                count--;
+            }
+            t=max(t,i-j+1);
+        }
+        return t;
+    }
+};
