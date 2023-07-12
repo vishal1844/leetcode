57560eec5c4508f516aa52d7c6bad0bc5880881f
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-07-13 02:11:20 +0530

    Time: 15 ms (65.65%), Space: 10 MB (36.34%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,47 +1,34 @@
 class Solution {
+    int a[4]={-1,1,0,0};
+    int b[4]={0,0,-1,+1};
+    bool dfs(int i,int j,int n,int m,vector<vector<int>>&grid,vector<vector<int>>&visit){
+        if(i<0||j<0||i>=n||j>=m)return false;
+        if(grid[i][j]==1||visit[i][j]==1)return true;
+        visit[i][j]=1;
+        bool flag=1;
+        for(int k=0;k<4;k++){
+            int newi=a[k]+i;
+            int newj=b[k]+j;
+            if(!dfs(newi,newj,n,m,grid,visit)){
+                flag=0;
+            }
+        }
+        return flag;
+    }
 public:
     int closedIsland(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<bool>> visit(m, vector<bool>(n));
-        int count = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0 && !visit[i][j] && bfs(i, j, m, n, grid, visit)) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>visit(n,vector<int>(m,0));
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!visit[i][j]&&grid[i][j]==0){
+                    if(dfs(i,j,n,m,grid,visit))
                     count++;
                 }
             }
         }
         return count;
-    }
-
-    bool bfs(int x, int y, int m, int n, vector<vector<int>>& grid, vector<vector<bool>>& visit) {
-        queue<pair<int, int>> q;
-        q.push({x, y});
-        visit[x][y] = 2;
-        bool isClosed = true;
-
-        vector<int> dirx{0, 1, 0, -1};
-        vector<int> diry{-1, 0, 1, 0};
-
-        while (!q.empty()) {
-            x = q.front().first;   // row nnumber
-            y = q.front().second;  // column number
-            q.pop();
-
-            for (int i = 0; i < 4; i++) {
-                int r = x + dirx[i];
-                int c = y + diry[i];
-                if (r < 0 || r >= m || c < 0 || c >= n) {
-                    // (x, y) is a boundary cell.
-                    isClosed = false;
-                } else if (grid[r][c] == 0 && !visit[r][c]) {
-                    q.push({r, c});
-                    visit[r][c] = true;
-                }
-            }
-        }
-
-        return isClosed;
     }
 };
