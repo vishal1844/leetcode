d3d347f325620568ea5cbcb7b33a2eb5042b4963
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-07-14 03:27:03 +0530

    Time: 15 ms (57.92%), Space: 10.6 MB (21.91%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,36 +1,48 @@
 class Solution {
-    int a[4]={0,0,-1,1};
-    int b[4]={-1,1,0,0};
-    bool bfs(int i,int j,int n,int m,vector<vector<char>>&grid,vector<vector<int>>&visit){
-        if(i<0||j<0||i>=n||j>=m)return 0;
-        if(grid[i][j]=='X'||visit[i][j])return 1;
-        bool flag=1;
-        grid[i][j]='X';
+    int a[4]={0,0,1,-1};
+    int b[4]={1,-1,0,0};
+    void bfs(int i,int j,int n,int m,vector<vector<char>>&grid,vector<vector<int>>&visit){
+        if(i<0||j<0||i>=n||j>=m)return;
+        if(grid[i][j]=='X'||visit[i][j])return;
+        grid[i][j]='Z';
         visit[i][j]=1;
         for(int k=0;k<4;k++){
             int newi=a[k]+i;
             int newj=b[k]+j;
-            if(!bfs(newi,newj,n,m,grid,visit)){
-                flag=0;
-                //return 0;
-            }
+            bfs(newi,newj,n,m,grid,visit);
         }
-        return flag;
     }
 public:
     void solve(vector<vector<char>>& board) {
         int n=board.size();
         int m=board[0].size();
-        vector<vector<int>>visit(n,vector<int>(m,0));
+        vector<vector<int>>visit(n,vector<int>(m));
+        vector<vector<char>>grid=board;
         for(int i=0;i<n;i++){
-           // vector<vector<char>>grid=board;
+            if(grid[i][0]=='O'&&!visit[i][0]){
+                bfs(i,0,n,m,grid,visit);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(grid[i][m-1]=='O'&&!visit[i][m-1]){
+                bfs(i,m-1,n,m,grid,visit);
+            }
+        }
+        for(int i=0;i<m;i++){
+            if(grid[n-1][i]=='O'&&!visit[n-1][i]){
+                bfs(n-1,i,n,m,grid,visit);
+            }
+        }
+        for(int i=0;i<m;i++){
+            if(grid[0][i]=='O'&&!visit[0][i]){
+                bfs(0,i,n,m,grid,visit);
+            }
+        }
+        
+        for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                vector<vector<char>>grid=board;
-                if(board[i][j]=='O'&&!visit[i][j]){
-                    if(bfs(i,j,n,m,grid,visit)){
-                        board=grid;
-                    }
-                    grid.clear();
+                if(grid[i][j]!='Z'){
+                    board[i][j]='X';
                 }
             }
         }
