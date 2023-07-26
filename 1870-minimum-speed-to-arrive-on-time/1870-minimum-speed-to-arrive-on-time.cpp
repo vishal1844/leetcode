fb62849fa2d15a9ea99ae3efb434258112dbc289
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-07-26 23:26:55 +0530

    Time: 364 ms (43.31%), Space: 101.4 MB (94.10%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
@@ -1,30 +1,33 @@
 class Solution {
     bool solve(vector<int>&dist,double hour,int mid){
-        int i,j,k,l;
-        double t=0;
+        double k,l,t=0;
+        int i,j;
         for(i=0;i<dist.size()-1;i++){
-            t+=ceil((1.0*dist[i])/mid);
+            k=ceil((dist[i]*1.00)/mid);
+            t+=k;
         }
-        t+=(dist[i])/(1.00*mid);
-        //cout<<t<<" "<<mid<<endl;
+        k=((1.00*dist[i])/mid);
+        t+=k;
+       // cout<<t<<" ";
         return t<=hour;
     }
 public:
     int minSpeedOnTime(vector<int>& dist, double hour) {
-        int l=1,h=1e7,i,j,k=1e8;
-        //sort(dist.begin(),dist.end());
+        int n=dist.size();
+        int l=1,h=1e9,t=INT_MAX;
         while(l<=h){
             int mid=l+(h-l)/2;
-            //cout<<mid<<" ";
-            if(solve(dist,hour,mid)){
-                k=mid;
+            if(solve(dist,hour,mid)==1){
+                t=min(t,mid);
                 h=mid-1;
             }
             else{
                 l=mid+1;
             }
         }
-        if(k==1e8)return -1;
-        return k;
+        if(t==INT_MAX){
+            return -1;
+        }
+        return t;
     }
 };
