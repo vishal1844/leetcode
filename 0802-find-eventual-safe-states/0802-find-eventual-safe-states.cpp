b03b2de99313af2ac10c76fd5c9843db58d18cb9
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-07-31 03:32:49 +0530

    Time: 171 ms (75.82%), Space: 60.1 MB (37.23%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,46 +1,47 @@
 class Solution {
-public:
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> indegree(n);
-        vector<vector<int>> adj(n);
-
-        for (int i = 0; i < n; i++) {
-            for (auto node : graph[i]) {
-                adj[node].push_back(i);
-                indegree[i]++;
-            }
-        }
-
-        queue<int> q;
-        // Push all the nodes with indegree zero in the queue.
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
-                q.push(i);
-            }
-        }
-
-        vector<bool> safe(n);
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-            safe[node] = true;
-
-            for (auto& neighbor : adj[node]) {
-                // Delete the edge "node -> neighbor".
-                indegree[neighbor]--;
-                if (indegree[neighbor] == 0) {
-                    q.push(neighbor);
+    private:
+    bool dfs(int node,vector<int>&visit,vector<int>&path,vector<int>adj[]){
+        visit[node]=1;
+        path[node]=1;
+        for(auto itr:adj[node]){
+            if(!visit[itr]){
+                if(dfs(itr,visit,path,adj)==true){
+                    return true;
                 }
             }
-        }
-
-        vector<int> safeNodes;
-        for(int i = 0; i < n; i++) {
-            if(safe[i]) {
-                safeNodes.push_back(i);
+            else if(path[itr]==1){
+                return true;
+            }
+            else{
+                path[itr]=0;
+                //break;
             }
         }
-        return safeNodes;
+        path[node]=0;
+        return false;
+    }
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int>adj[n];
+        vector<int>visited(n,0),path(n,0);
+        for(int i=0;i<n;i++){
+            for(auto itr:graph[i]){
+                adj[i].push_back(itr);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                dfs(i,visited,path,adj);
+            }
+        }
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(path[i]==0){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+        
     }
 };
