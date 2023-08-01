d8efd8600dbfd8567e8a577db96b1f637f88b1dc
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-08-01 15:49:10 +0530

    Time: 18 ms (87.68%), Space: 9.9 MB (49.76%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,24 +1,23 @@
 class Solution {
-    void solve(int ind,int n,int k,vector<vector<int>>&ans,vector<int>&vec,vector<int>v){
-        if(v.size()==k){
-            ans.push_back(v);
+    vector<vector<int>>ans;
+    void solve(int i,int n,int k,vector<int>&v){
+        if(v.size()==k||i>n){
+            if(v.size()==k){
+                ans.push_back(v);
+            }
             return;
         }
-        for(int i=ind;i<n;i++){
-            v.push_back(vec[i]);
-            solve(i+1,n,k,ans,vec,v);
+        for(int l=i;l<=n;l++){
+            v.push_back(l);
+            solve(l+1,n,k,v);
             v.pop_back();
         }
     }
+    
 public:
     vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>>ans;
-        vector<int>vec,v;
-        // vector<int>visit(n,0);
-        for(int i=1;i<=n;i++){
-            vec.push_back(i);
-        }
-        solve(0,n,k,ans,vec,v);
+        vector<int>v;
+        solve(1,n,k,v);
         return ans;
     }
 };
