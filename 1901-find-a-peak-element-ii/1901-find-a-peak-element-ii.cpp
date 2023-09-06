72e886895692f3448217a93212e2b014743f4998
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-09-06 17:14:24 +0530

    Time: 133 ms (71.36%), Space: 46.4 MB (26.78%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,29 +1,32 @@
 class Solution {
-    int newin=-1,newjn=-1,t=0;
-    int a[4]={1,-1,0,0};
-    int b[4]={0,0,1,-1};
-    void solve(int i,int j,int n,int m,vector<vector<int>>&mat,vector<vector<int>>&visit){
-        if(t<mat[i][j]){
-            t=mat[i][j];
-            newin=i;
-            newjn=j;
-        }
-        visit[i][j]=1;
-        for(int k=0;k<4;k++){
-            int newi=a[k]+i;
-            int newj=b[k]+j;
-            if(newi>=0&&newj>=0&&newi<n&&newj<m&&visit[newi][newj]==0){
-                solve(newi,newj,n,m,mat,visit);
+    bool solve(int mid,int fcol,int lcol,vector<vector<int>>&mat){
+        int maxirow=0;
+        for(int i=0;i<mat.size();i++){
+            if(mat[maxirow][mid]<mat[i][mid]){
+                maxirow=i;
             }
         }
+        if(mid+1<=lcol&&mat[maxirow][mid]>mat[maxirow][mid+1]){
+            return 1;
+        }
+        return 0;
     }
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        int n=mat.size();
-        int m=mat[0].size();
-        vector<vector<int>>visit(n,vector<int>(m,0));
-         solve(0,0,n,m,mat,visit);
-        vector<int>v{newin,newjn};
+        int fcol=0,lcol=mat[0].size()-1;
+        while(fcol<lcol){
+            int mid=fcol+(lcol-fcol)/2;
+            if(solve(mid,fcol,lcol,mat))lcol=mid;
+            else
+                fcol=mid+1;
+        }
+        int maxirow=0;
+        for(int i=0;i<mat.size();i++){
+            if(mat[maxirow][fcol]<mat[i][fcol]){
+                maxirow=i;
+            }
+        }
+        vector<int>v{maxirow,fcol};
         return v;
     }
 };
