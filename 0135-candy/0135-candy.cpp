9b0cb90fe548cec9240569ab24b90d823a6f988f
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-09-14 04:20:01 +0530

    Time: 14 ms (83.03%), Space: 18.2 MB (15.67%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int i,j,k,l,m,t=0;
         int n=ratings.size();
-        if(n==1)return 1;
-        int sum=n;
-        for(i=1;i<n;){
-            if(ratings[i]==ratings[i-1]){
-                i++;
-                continue;
+        int t=0;
+        vector<int>v(n,1);
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                v[i]=v[i-1]+v[i];
             }
-            int peak=0;
-            while(i<n&&ratings[i]>ratings[i-1]){
-                peak++;
-                sum+=peak;
-                i++;
-            }
-            int valley=0;
-            while(i<n&&ratings[i-1]>ratings[i]){
-                valley++;
-                sum+=valley;
-                i++;
-            }
-            sum-=min(peak,valley);
         }
-        return sum;
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                v[i]=max(v[i],v[i+1]+1);
+            }
+        }
+        for(int i=0;i<n;i++){
+            t+=v[i];
+        }
+        return t;
     }
 };
