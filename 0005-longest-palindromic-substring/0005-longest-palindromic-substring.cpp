2f611359c44e9336fe5f7be51c0349348996e0c7
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-11-01 22:49:10 +0530

    Time: 52 ms (54.85%), Space: 162.8 MB (21.32%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,73 +1,23 @@
 class Solution {
-private: 
-//     bool solve(vector<vector<bool>> &dp, int i, int j, string &s){
-//         if(i == j){
-//             return dp[i][j] = true;
-//         }
-//         if(j-i == 1){
-//             if(s[i] == s[j]){
-//                 return dp[i][j] = true;
-//             }
-//             else{
-//                 return dp[i][j] = false;
-//             }
-//         }
-//         if(s[i] == s[j] && dp[i+1][j-1] == true){
-//             return dp[i][j] = true;
-//         } else {
-//             return dp[i][j] = false;
-//         }
-//     }
-// public:
-//     string longestPalindrome(string s) {
-//         int n = s.size();
-//         int startIndex = 0; int maxlen = 0;
-//         vector<vector<bool>> dp(n, vector<bool>(n, false));
-//         for(int g=0; g<n; g++){
-//             for(int i=0, j=g; j<n; i++, j++){
-//                 solve(dp, i, j, s);
-//                 if(dp[i][j] == true){
-//                     if(j-i+1 > maxlen){
-//                         startIndex = i;
-//                         maxlen = j-i+1;
-//                     }
-//                 }
-//             }
-//         }
-//         return s.substr(startIndex, maxlen);
-//     }
-    int check(string &s, int L, int R)
-    {
-        while(L>=0 and R<s.length() and s[L]==s[R])
-        {
-            L--;
-            R++;
+    int solve(string s,int l,int r){
+        while(l>=0&&r<s.size()&&s[l]==s[r]){
+            l--;
+            r++;
         }
-        // cout<<"r="<<R<<"l="<<L;
-         //cout<<R-L-1<<" ";
-        return (R-1)-(L+1)+1;
+        return r-l-1;
     }
-    public:
-    
+public:
     string longestPalindrome(string s) {
-        
-        int ans = 0, st=0;
-        int n = s.length();
-        
-        for(int i = 0;i<n;i++)
-        {
-            int len1 = check(s, i,i);
-            int len2 = check(s, i, i+1);
-            
-            int len = max(len1, len2);
-            
-            if(len> ans)
-            {
-                ans = len;
-                st = i-(len-1)/2;
-                //cout<<i<<st<<" ";
+        int t=0,ans=0,j,k;
+        for(int i=0;i<s.size();i++){
+            j=solve(s,i,i);
+            k=solve(s,i,i+1);
+            j=max(j,k);
+            if(ans<j){
+                ans=j;
+                t=i-(j-1)/2;
             }
         }
-        return s.substr(st, ans);
-    }
+        return s.substr(t,ans);
+    }
 };
