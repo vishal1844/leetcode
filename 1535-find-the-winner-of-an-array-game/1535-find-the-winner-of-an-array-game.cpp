79f7be9851a97b9adea522d5d71dd499761e8d32
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-11-06 01:31:48 +0530

    Time: 88 ms (56.88%), Space: 63.3 MB (86.24%) - LeetHub

---
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int getWinner(vector<int>& arr, int k) {
+        int i,j,l=arr[0],m,t=0;
+        int n=arr.size();
+        for(i=1;i<n;i++){
+            if(l>arr[i]){
+                t++;
+                if(t==k)return l;
+            }
+            else{
+                if(t==k)return l;
+                break;
+            }
+        }
+        if(i==n)return l;
+        t=1;
+        l=arr[i];
+        i++;
+        for(;i<n;i++){
+            if(l>arr[i]){
+                t++;
+                if(t==k) return l;
+            }
+            else{
+                if(t==k)return l;
+                t=1;
+                l=arr[i];
+            }
+        }
+        return l;
+    }
+};
