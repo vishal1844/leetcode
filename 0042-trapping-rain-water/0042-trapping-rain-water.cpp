09146a03e7588b4b7c6d994376d9e9ca1dfca52c
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-11-09 18:05:42 +0530

    Time: 15 ms (29.30%), Space: 20 MB (74.87%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     int trap(vector<int>& h) {
-        int i,j,k,l,m,t=0;
-        int n=h.size();
-        vector<int>p(n,0),s(n,0);
-        for(i=0;i<n;i++){
-            t=max(t,h[i]);
-            p[i]=t;
+        int l=0,r=h.size()-1,maxil=0,maxir=0,res=0;
+        while(l<=r){
+            if(h[l]<=h[r]){
+                if(maxil<=h[l]){
+                    maxil=h[l];
+                }
+                else{
+                    res+=(maxil-h[l]);
+                }
+                l++;
+            }
+            else{
+                if(maxir<=h[r]){
+                    maxir=h[r];
+                }
+                else{
+                    res+=(maxir-h[r]);
+                }
+                r--;
+            }
         }
-        t=0;
-        for(i=n-1;i>=0;i--){
-            t=max(t,h[i]);
-            s[i]=t;
-        }
-        l=0;
-        t=0;
-        for(i=0;i<n;i++){
-            t+=min(p[i],s[i])-h[i];
-        }
-        return t;
-        
+        return res;
     }
 };
