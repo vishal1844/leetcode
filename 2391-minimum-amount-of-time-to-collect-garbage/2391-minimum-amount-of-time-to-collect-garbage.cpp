04a8ed6ba9c8a2998a59fdbcbf86d9f6fd753db2
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-11-21 15:55:34 +0530

    Time: 176 ms (37.73%), Space: 104.1 MB (9.11%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    int garbageCollection(vector<string>& gar, vector<int>& tra) {
-        int cnt=0;
-        int last_g=0, last_p=0, last_m=0;
-            
-        for(int i=0; i<gar.size(); i++)
-        {
-            for(auto it:gar[i])
-            {
-                if(it=='M') last_m=i;
-                else if(it=='P') last_p=i;
-                else last_g=i;
-                cnt++; 
+    int garbageCollection(vector<string>& gar, vector<int>& tr) {
+        int n=gar.size();
+        int m=tr.size();
+        int i,j,k,l,t=0;
+        vector<int>pre(m+1,0);
+        for(i=0;i<m;i++){
+            pre[i+1]=tr[i]+pre[i];
+        }
+        k=0;
+        l=0;
+        int p=0;
+        for(i=0;i<n;i++){
+            for(j=0;j<gar[i].size();j++){
+                t++;
+                if(gar[i][j]=='M'){
+                    k=i;
+                }
+                else if(gar[i][j]=='G')l=i;
+                else p=i;
             }
         }
-        
-        int tot=cnt;
-        int sum=0;
-        for(int i=0; i<tra.size(); i++)
-        {
-            sum += tra[i];
-            if(last_g==i+1) tot += sum;
-            if(last_p==i+1) tot += sum;
-            if(last_m==i+1) tot += sum;   
-        }
-        return tot;
+        t+=pre[k]+pre[l]+pre[p];
+        return t;
     }
 };
