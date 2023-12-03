ce7869e7a563a28315df607ef285f83c287d0dd9
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-12-03 15:09:26 +0530

    Time: 196 ms (54.78%), Space: 66.4 MB (31.92%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    int isit(int i,string &st,unordered_map<string,int>&mp,int n,vector<int>&dp){
+    int isit(int i,string&st,unordered_map<string,int>&mp,int n,vector<int>&dp){
         if(i==n)
             return 1;
-        if(mp.count(st))return 1;
         if(dp[i]!=-1)
             return dp[i];
         string temp="";
-        int j;
-        for(j=i;j<n;j++){
+        int j=i;
+        for(j;j<n;j++){
             temp+=st[j];
-            string s=st.substr(j+1);
             if(mp.count(temp)){
-                if(mp.count(s)||isit(j+1,st,mp,n,dp))
+                if(isit(j+1,st,mp,n,dp))
                     return dp[i]=1;
             }
         }
@@ -21,25 +19,19 @@ public:
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
         unordered_map<string,int>mp;
         vector<string>ans;
-        for(auto itr: words){
-            mp[itr]++;
+        vector<pair<int,string>>vec;
+        for(auto it:words)
+        {
+            vec.push_back({it.size(),it});
         }
-        //vector<pair<int,string>>vec;
-        // for(auto it:words)
-        // {
-        //     vec.push_back({it.size(),it});
-        // }
-        //sort(vec.begin(),vec.end());
-        for(auto it:words){
-            vector<int>dp(it.size(),-1);
-            //string s=it;
-            mp.erase(it);
-            if(isit(0,it,mp,it.size(),dp)){
-                ans.push_back(it);
+        sort(vec.begin(),vec.end());
+        for(auto it:vec){
+            vector<int>dp(it.second.size(),-1);
+            if(isit(0,it.second,mp,it.second.size(),dp)){
+                ans.push_back(it.second);
             }
-            mp[it]++;
                 
-            //mp[it.second]++;
+            mp[it.second]++;
         }
         return ans;
     }
