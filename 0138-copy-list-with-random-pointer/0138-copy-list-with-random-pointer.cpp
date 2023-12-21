bf4a8322a10184a72d63faf724a648de20e82a03
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-12-22 01:40:17 +0530

    Time: 8 ms (27.64%), Space: 11.5 MB (87.06%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,18 +17,64 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        Node*tail=head;
-        map<Node*,Node*>mp;
-        while(tail){
-            mp[tail]=new Node(tail->val);
-            tail=tail->next;
+    //     Node*tail=head;
+    //     Node*temp=head;
+    //     tail=tail->next;
+    //     while(tail){
+    //         Node* copy=new Node(temp->val);
+    //         temp->next=copy;
+    //         copy->next=tail;
+    //         tail=tail->next->next;
+    //         //temp=temp->next;
+    //     }
+    //     tail=head;
+    //     while(tail){
+    //         if(tail->random)
+    //         tail->next->random=tail->random;
+    //         tail=tail->next->next;
+    //     }
+    //     tail=head;
+    //     Node*dummy=new Node(0);
+    //     Node*copy=dummy;
+    //     while(tail){
+    //         temp=tail->next->next;
+    //         dummy->next=tail->next;
+    //         dummy=dummy->next;
+    //         tail=temp;
+    //     }
+    //     return copy->next;
+    // }
+        if (!head) {
+        return nullptr;
+    }
+    Node* temp = head;
+    while (temp) {
+        Node* copy = new Node(temp->val);
+        copy->next = temp->next;
+        temp->next = copy;
+        temp = copy->next;
+    }
+    temp = head;
+    while (temp) {
+        if (temp->random) {
+            temp->next->random = temp->random->next;
         }
-        tail=head;
-        while(tail){
-            mp[tail]->next=mp[tail->next];
-            mp[tail]->random=mp[tail->random];
-            tail=tail->next;
+        temp = temp->next->next;
+    }
+    Node* original = head;
+    Node* copyHead = head->next;
+    Node* copy = copyHead;
+
+    while (original) {
+        original->next = original->next->next;
+        original = original->next;
+
+        if (copy->next) {
+            copy->next = copy->next->next;
+            copy = copy->next;
         }
-        return mp[head];
+    }
+
+    return copyHead;
     }
 };
