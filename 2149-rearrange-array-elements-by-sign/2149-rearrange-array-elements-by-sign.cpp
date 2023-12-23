372806a353e65e2736a7330f6ebb847465789424
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-12-23 18:18:09 +0530

    Time: 169 ms (37.25%), Space: 124.2 MB (87.42%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -4,19 +4,24 @@ public:
         vector<int>v(nums.size(),0);
         int i,j,k,l,m,t=0;
         j=0;
+        k=1;
         for(i=0;i<nums.size();i++){
             if(nums[i]>0){
                 v[j]=nums[i];
                 j+=2;
             }
-        }
-        j=1;
-        for(i=0;i<nums.size();i++){
-            if(nums[i]<0){
-                v[j]=nums[i];
-                j+=2;
+            else{
+                v[k]=nums[i];
+                k+=2;
             }
         }
+        // j=1;
+        // for(i=0;i<nums.size();i++){
+        //     if(nums[i]<0){
+        //         v[j]=nums[i];
+        //         j+=2;
+        //     }
+        // }
         return v;
     }
 };
