83ce2654230a45b67fc7b6e79e0dd2853f2128be
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2023-12-30 10:38:39 +0530

    Time: 14 ms (95.48%), Space: 7.6 MB (88.10%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,32 +1,23 @@
-#define INF 10000000
+int dp[301][11];
 class Solution {
-public:
-    int n;
-    int dp[305][15];
-    int dfs(int i,int d,vector <int> &jd)
-    {
-        if(i==n && d==0) 
+    int solve(int i,int n,vector<int>&v,int d){
+        if(i==n&&d==0){
             return 0;
-        if(i==n || d==0 || n-i<d) 
-            return INF;
-        if(dp[i][d]!=-1) 
-            return dp[i][d];
-        int ans=INF;
-        int maxele=-INF;
-        for(int j=i;j<n;j++)
-        {   
-            maxele=max(maxele,jd[j]); 
-            ans=min(ans,maxele+dfs(j+1,d-1,jd));
         }
-        dp[i][d]=ans;
-        return ans;
+        if(i==n||d==0||n-i<d)return INT_MAX/2;
+        if(dp[i][d]!=-1)return dp[i][d];
+        int maxi=0;
+        int mini=INT_MAX/2;
+        for(int j=i;j<n;j++){
+            maxi=max(maxi,v[j]);
+            mini=min(mini,(maxi+solve(j+1,n,v,d-1)));
+        }
+        return dp[i][d]=mini;
     }
-    int minDifficulty(vector<int>& jobDifficulty, int d) {
-        n=jobDifficulty.size();
-        if(n<d)       //not possible
-            return -1;
+public:
+    int minDifficulty(vector<int>& j, int d) {
+        if(j.size()<d)return -1;
         memset(dp,-1,sizeof(dp));
-        int ans= dfs(0,d,jobDifficulty);
-        return ans;
+        return solve(0,j.size(),j,d);
     }
 };
