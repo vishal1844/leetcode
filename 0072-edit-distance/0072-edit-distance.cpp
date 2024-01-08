9b6537f8b2c6c3f3eab4ed874d818964f318ec92
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-01-09 02:30:44 +0530

    Time: 4 ms (92.78%), Space: 9.4 MB (31.17%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,40 +1,20 @@
-class Solution
-{
-public:
-    vector<vector<int>> memo;
-    int minDistance(string word1, string word2)
-    {
-        memo.resize(word1.length() + 1, vector<int>(word2.length() + 1, -1));
-        return minDistanceRecur(word1, word2, word1.size(), word2.size());
+class Solution {
+    int solve(string&word1,string &word2,int n,int m,vector<vector<int>>&dp){
+        if(n<=0)return m;
+        if(m<=0)return n;
+        if(dp[n][m]!=-1)return dp[n][m];
+        int mini=0;
+        if(word1[n-1]==word2[m-1]){
+            mini=solve(word1,word2,n-1,m-1,dp);
+        }
+        else{
+            mini=min({solve(word1,word2,n-1,m-1,dp),solve(word1,word2,n-1,m,dp),solve(word1,word2,n,m-1,dp)})+1;
+        }
+        return dp[n][m]=mini;
     }
-
-    int minDistanceRecur(string &word1, string &word2, int word1Index, int word2Index)
-    {
-        if (word1Index == 0)
-        {
-            return word2Index;
-        }
-        if (word2Index == 0)
-        {
-            return word1Index;
-        }
-        if (memo[word1Index][word2Index] != -1)
-        {
-            return memo[word1Index][word2Index];
-        }
-        int minEditDistance = 0;
-        if (word1[word1Index - 1] == word2[word2Index - 1])
-        {
-            minEditDistance = minDistanceRecur(word1, word2, word1Index - 1, word2Index - 1);
-        }
-        else
-        {
-            int insertOperation = minDistanceRecur(word1, word2, word1Index, word2Index - 1);
-            int deleteOperation = minDistanceRecur(word1, word2, word1Index - 1, word2Index);
-            int replaceOperation = minDistanceRecur(word1, word2, word1Index - 1, word2Index - 1);
-            minEditDistance = min(insertOperation, min(deleteOperation, replaceOperation)) + 1;
-        }
-        memo[word1Index][word2Index] = minEditDistance;
-        return minEditDistance;
+public:
+    int minDistance(string word1, string word2) {
+        vector<vector<int>>dp(word1.size()+1,vector<int>(word2.size()+1,-1));
+        return solve(word1,word2,word1.size(),word2.size(),dp);
     }
 };
