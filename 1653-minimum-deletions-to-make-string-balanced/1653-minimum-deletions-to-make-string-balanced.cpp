ca4cf51eee8846459c120db156c170df1cc5311b
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-01-09 02:12:25 +0530

    Time: 92 ms (78.05%), Space: 22.6 MB (69.38%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int i,j,k=0,l,m=0,t=0;
-        vector<int>v(s.size(),0);
-        vector<int>vec(s.size(),0);
+        int i,j,k,l=0,m=0,t=0;
         for(i=0;i<s.size();i++){
-            v[i]=m;
-            if(s[i]=='b')
-            m++;
-        }
-        m=0;
-        for(i=s.size()-1;i>=0;i--){
-            vec[i]=m;
-            if(s[i]=='a')
-            m++;
+            if(s[i]=='a')m++;
         }
         t=s.size();
         for(i=0;i<s.size();i++){
-            t=min(t,v[i]+vec[i]);
+            if(s[i]=='a')m--;
+            t=min(t,m+l);
+            if(s[i]=='b')l++;
         }
         return t;
     }
