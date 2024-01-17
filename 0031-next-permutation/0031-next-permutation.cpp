4e97954c9a9421f4d525b11c21cc47089accbb8c
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-01-18 02:22:58 +0530

    Time: 8 ms (17.42%), Space: 12.6 MB (22.77%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        //int k=nums.size();
-       // cout<<k<<" ";
-        next_permutation(nums.begin(),nums.end());
-        // for(int i=0;i<nums.size();i++){
-        //     cout<<nums[i]<<" ";
-        // }
+        int i,j,k,l,m,t=0;
+        int n=nums.size();
+        for(i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1])break;
+        }
+        if(i<0)reverse(nums.begin(),nums.end());
+        else{
+            for(j=n-1;j>i;j--){
+                if(nums[j]>nums[i])break;
+            }
+            swap(nums[i],nums[j]);
+            reverse(nums.begin()+i+1,nums.end());
+        }
     }
 };
