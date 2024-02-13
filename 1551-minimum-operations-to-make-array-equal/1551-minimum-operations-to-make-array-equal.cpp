4997d808eac91397725aa87894635da22f2fa942
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-02-13 16:28:33 +0530

    Time: 4 ms (21.22%), Space: 7.3 MB (21.93%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -3,22 +3,19 @@ public:
     int minOperations(int n) {
         int i,j,k,l,m;
         long long t=0;
-        vector<int>v(n,0);
+       // vector<int>v(n,0);
         for(i=0;i<n;i++){
             k=2*i+1;
             //t+=k;
-            v[i]=k;
+            if(k<n)
+            t+=k;
+            else break;
         }
-        t=n;
-        //t=sqrt(t);
-        //cout<<t<<" ";
-        l=0;
-        i=0;
-        j=n-1;
-        while(v[i]<t){
-            l+=abs(v[i]-t);
-            i++;
-        }
-        return l;
+        long long p=n*i;
+        p-=t;
+        return p;
     }
 };
+
+
+// /* TC-O(n) where n=length of array;
