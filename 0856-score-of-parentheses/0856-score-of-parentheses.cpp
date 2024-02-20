d8f1c8eae45b4272e0ed91ab0340f0966bda639a
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-02-20 10:34:20 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (9.77%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.cpp
+++ 0856-score-of-parentheses/0856-score-of-parentheses.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int scoreOfParentheses(string s) {
-        int i,j=0,t=0;
+        int i,j,k,l,t=0;
+        stack<int>st;
+        st.push(0);
         for(i=0;i<s.size();i++){
             if(s[i]=='('){
-                j++;
+                st.push(0);
             }
             else{
-                j--;
-                if(s[i-1]=='('){
-                t+=(1<<j);
-                }
+                k=st.top();
+                st.pop();
+                l=st.top();
+                st.pop();
+                st.push(l+max(2*k,1));
             }
         }
-        return t;
-        
+        return st.top();
     }
 };
