f940c9780719363cc92b52bf52f24dc9f2e0fb41
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-04-01 16:56:03 +0530

    Time: 88 ms (63.40%), Space: 82.6 MB (54.90%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int minK, int maxK) {
-        long long int i,j,k=0,l=0,m=0,t=0;
-        bool s=0,p=0;
+        long long i,j=0,k=-1,l=-1,m,t=0;
         for(i=0;i<nums.size();i++){
-            if(nums[i]<minK||nums[i]>maxK){
-                k=i+1;
-                s=0;
-                p=0;
-            }
             if(nums[i]==minK){
-                l=i;
-                s=1;
+                k=i;
             }
             if(nums[i]==maxK){
-                m=i;
-                p=1;
+                l=i;
             }
-            if(s&&p){
-                t+=(min(l,m)-k+1);
+            if(nums[i]<minK||nums[i]>maxK){
+                k=-1;
+                l=-1;
+                j=i+1;
+            }
+            if(k!=-1&&l!=-1){
+                t+=min(k,l)-j+1;
             }
         }
         return t;
