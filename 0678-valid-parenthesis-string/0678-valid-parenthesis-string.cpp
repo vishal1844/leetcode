d14ac75a119dcb9baf942585d97d269538eeb793
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-04-08 15:07:19 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (92.96%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,15 +1,33 @@
+int dp[101][101];
 class Solution {
+    int solve(int i,int n,string&s,int t){
+        if(i>=n){
+            //cout<<"UUJI"<<t<<" ";
+            if(t==0)return 1;
+            return 0;
+        }
+        if(dp[i][t]!=-1)return dp[i][t];
+        int k=0;
+        if(s[i]=='('){
+            k|=solve(i+1,n,s,t+1);
+        }
+        else if(s[i]=='*'){
+            k|=solve(i+1,n,s,t+1);
+            if(t){
+                k|=solve(i+1,n,s,t-1);
+            }
+                k|=solve(i+1,n,s,t);
+        }
+        else{
+            if(t>0){
+                k|=solve(i+1,n,s,t-1);
+            }
+        }
+        return dp[i][t]=k;
+    }
 public:
     bool checkValidString(string s) {
-        int i,j,k,l,m=0,t=0;
-        int n=s.size();
-        for(i=0;i<s.size();i++){
-            if(s[i]=='('||s[i]=='*')m++;
-            else m--;
-            if(s[n-i-1]==')'||s[n-i-1]=='*')t++;
-            else t--;
-            if(t<0||m<0)return 0;
-        }
-        return 1;
+        memset(dp,-1,sizeof(dp));
+        return solve(0,s.size(),s,0);
     }
 };
