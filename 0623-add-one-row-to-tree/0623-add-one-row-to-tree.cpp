5c33a8a4c7d4d9864342039048c20f5c0c248818
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-04-18 14:10:44 +0530

    Time: 14 ms (46.22%), Space: 24.4 MB (39.91%) - LeetHub

--- 0623-add-one-row-to-tree/0623-add-one-row-to-tree.cpp
+++ 0623-add-one-row-to-tree/0623-add-one-row-to-tree.cpp
@@ -12,40 +12,51 @@
 class Solution {
 public:
     TreeNode* addOneRow(TreeNode* root, int val, int depth) {
-         if (depth == 1) {
-        TreeNode* newRoot = new TreeNode(val);
-        newRoot->left = root;
-        return newRoot;
-    }
-
-    queue<TreeNode*> q;
-    q.push(root);
-    int currentDepth = 1;
-
-    while (!q.empty()) {
-        int n = q.size();
-        for (int i = 0; i < n; i++) {
-            TreeNode* node = q.front();
-            q.pop();
-
-            if (currentDepth == depth - 1) {
-                TreeNode* leftChild = node->left;
-                TreeNode* rightChild = node->right;
-
-                node->left = new TreeNode(val);
-                node->left->left = leftChild;
-
-                node->right = new TreeNode(val);
-                node->right->right = rightChild;
-            } else {
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
+        TreeNode*temp=root;
+        int i,j,k,l,t=0;
+        if(depth==1){
+            temp=new TreeNode(val);
+            temp->left=root;
+            temp->right=NULL;
+            return temp;
+        }
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            if(depth>2){
+                for(i=0;i<n;i++){
+                    TreeNode* itr=q.front();
+                    q.pop();
+                    if(itr->left){
+                        q.push(itr->left);
+                    }
+                    if(itr->right){
+                        q.push(itr->right);
+                    }
+                }
+                depth--;
+            }
+            else{
+                for(i=0;i<n;i++){
+                    TreeNode*itr=q.front();
+                    q.pop();
+                    TreeNode*l=itr->left;
+                    TreeNode*r=itr->right;
+                    //if(l){
+                        temp=new TreeNode(val);
+                        itr->left=temp;
+                        temp->left=l;
+                   // }
+                    //if(r){
+                        temp=new TreeNode(val);
+                        itr->right=temp;
+                        temp->right=r;
+                    //}
+                }
+                break;
             }
         }
-        if (currentDepth == depth - 1) break;
-        currentDepth++;
-    }
-
-    return root;
+        return root;
     }
 };
