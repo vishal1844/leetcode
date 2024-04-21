a40529f6140da88ff0a1c4eda8583635fe7f2a20
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-04-22 02:57:07 +0530

    Time: 102 ms (70.17%), Space: 67.7 MB (65.83%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -1,57 +1,36 @@
 class Solution {
+    int a[4] = {-1, 1, 0, 0};
+    int b[4] = {0, 0, -1, 1};
+
+    void solve(int i, int j, vector<vector<int>>& land, int& l, int& r) {
+        land[i][j] = 0;
+        l = max(l, i);
+        r = max(r, j);
+
+        for (int p = 0; p < 4; p++) {
+            int newi = i + a[p];
+            int newj = j + b[p];
+
+            if (newi >= 0 && newj >= 0 && newi < land.size() && newj < land[0].size() && land[newi][newj] == 1) {
+                solve(newi, newj, land, l, r);
+            }
+        }
+    }
+
 public:
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
-        vector<vector<bool>> visited(land.size(), vector<bool>(land[0].size(), false));
-        vector<vector<int>> ans;
-        
-        for (int row1 = 0; row1 < land.size(); row1++) {
-            for (int col1 = 0; col1 < land[0].size(); col1++) {
-                if (land[row1][col1] && !visited[row1][col1]) {
-                    queue<pair<int, int>> q;
-                    q.push({row1, col1});
-                    visited[row1][col1] = true;
-                    
-                    pair<int, int> last = BFS(q, land, visited, row1, col1);
-                    ans.push_back({row1, col1, last.first, last.second});
+        vector<vector<int>> v;
+
+        for (int i = 0; i < land.size(); i++) {
+            for (int j = 0; j < land[0].size(); j++) {
+                if (land[i][j] == 1) {
+                    int l = i, r = j;
+                    solve(i, j, land, l, r);
+                    v.push_back({i, j, l , r });
                 }
             }
         }
-        
-        return ans;
-    }
-private:    
-    // The four directions in which traversal will be done.
-    int dirs[4][2] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
 
-    // Returns true if the coordinate is within the boundary of the matrix.
-    bool isWithinFarm(int x, int y, int N, int M) {
-        return x >= 0 && x < N && y >= 0 && y < M;
-    }
-    
-    pair<int, int> BFS(queue<pair<int, int>> q, vector<vector<int>>& land,
-        vector<vector<bool>>& visited, int x, int y) {
-        pair<int, int> curr;
-        
-        while (!q.empty()) {
-            curr = q.front();
-            q.pop();
-
-            int x = curr.first;
-            int y = curr.second;
-            
-            for (auto dir : dirs) {
-                // Neighbor cell coordinates.
-                int newX = x + dir[0], newY = y + dir[1];
-
-                // If the neighbor is within the matrix and is a farmland cell and not visited yet.
-                if (isWithinFarm(newX, newY, land.size(), land[0].size()) && !visited[newX][newY]
-                        && land[newX][newY]) {
-                    visited[newX][newY] = true;
-                    q.push({newX, newY});
-                }
-            }
-        }
-        
-        return curr;
+        return v;
     }
 };
