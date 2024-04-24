24e8b60da5bc49379507fd500df05f1df879fdca
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-04-24 09:33:00 +0530

    Time: 107 ms (70.95%), Space: 58.7 MB (56.36%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.cpp
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -1,43 +1,46 @@
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        if(n==0)
-            return {};
-        if(n==1)
-            return {0};
-        vector<int>res;
-        vector<int>degrees(n,0);
-        vector<vector<int>>adj(n);
-        for(int i=0;i<edges.size();i++)
-        {
-            adj[edges[i][0]].push_back(edges[i][1]);//creating adjacent list
-            adj[edges[i][1]].push_back(edges[i][0]);
-            degrees[edges[i][1]]++;//updating how many edges each node has
-            degrees[edges[i][0]]++;
+        vector<int>ans;
+        int i,j,k,l,m,t=0;
+        vector<int>adj[n];
+        vector<int>indegree(n);
+        for(auto itr:edges){
+            adj[itr[0]].push_back(itr[1]);
+            adj[itr[1]].push_back(itr[0]);
+            indegree[itr[0]]++;
+            indegree[itr[1]]++;
         }
-        queue<int>queue;
-        for(int i=0;i<n;i++)
-        {
-            if(degrees[i]==1)//adding all the leave nodes
-                queue.push(i);
-        }
-        while(!queue.empty())
-        {
-            res.clear();// clear vector before we start traversing level by level.
-            int size=queue.size();
-            for(int i=0;i<size;i++)
-            {
-                int cur=queue.front();
-                queue.pop();
-                res.push_back(cur);//adding nodes to vector.Goal is to get a vector of  just 1 or 2 nodes available.
-                for(auto &neighbor:adj[cur])
-                {
-                    degrees[neighbor]--;//removing current leave nodes
-                    if(degrees[neighbor]==1)//adding current leave nodes
-                        queue.push(neighbor);
-                }
+        queue<int>q;
+        vector<int>visit(n,0);
+        for(i=0;i<n;i++){
+            if(adj[i].size()==1){
+                q.push(i);
+                visit[i]=1;
             }
         }
-        return res;
+        while(!q.empty()){
+            m=q.size();
+            ans.clear();
+            for(i=0;i<m;i++){
+                auto itr=q.front();
+                q.pop();
+                indegree[itr]--;
+                for(auto it:adj[itr]){
+                    // if(!visit[it]){
+                    //     visit[it]=1;
+                    // }
+                    indegree[it]--;
+                    if(indegree[it]==1){
+                        q.push(it);
+                    }
+                }
+                ans.push_back(itr);
+            }   
+        }
+        if(ans.size()==0){
+            ans.push_back(0);
+        }
+        return ans;
     }
 };
