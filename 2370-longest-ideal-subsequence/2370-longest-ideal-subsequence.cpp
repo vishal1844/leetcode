81126534a7ae1208b49948ee99e55410260ce0cc
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-04-26 03:31:20 +0530

    Time: 246 ms (45.61%), Space: 31.1 MB (43.93%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -1,32 +1,25 @@
-int dp[100003][26]; // dp array with 26 characters
-
+int dp[100003][26];
 class Solution {
-    int solve(int i, int n, string& s, int k, int ch,int l) {
-        if (i >= n) return 0;
-        if (dp[i][ch] != -1) {
-            return dp[i][ch];
+    int solve(int i,int n,string&s,int k,char ch){
+        if(i>=n)return 0;
+        if(ch!='1')
+        if(dp[i][ch-'a']!=-1)return dp[i][ch-'a'];
+        int t=0;
+        if(ch=='1'){
+            t= 1+solve(i+1,n,s,k,s[i]);
         }
-        int t = 0;
-        if (ch + 'a' != s[i]&&l==1) {
-            t = solve(i + 1, n, s, k, ch,l);
-        } else {
-            l=0;
-            if (abs(s[i] - 'a' - ch) <= k) {
-                t = max(t, 1 + solve(i + 1, n, s, k, s[i] - 'a',l));
-            }
-            t = max(t, solve(i + 1, n, s, k, ch,l));
+        else if(abs(ch-s[i])<=k){
+            t=max(t,1+solve(i+1,n,s,k,s[i]));
         }
-        return dp[i][ch] = t;
+        t=max(t,solve(i+1,n,s,k,ch));
+        if(ch=='1'){
+            ch='a';
+        }
+        return dp[i][ch-'a']=t;
     }
-
 public:
     int longestIdealString(string s, int k) {
-        memset(dp, -1, sizeof(dp));
-        int maxLen = 0;
-        for (int i = 0; i < 26; i++) {
-            int l=1;
-            maxLen = max(maxLen, solve(0, s.size(), s, k, i,l));
-        }
-        return maxLen;
+        memset(dp,-1,sizeof(dp));
+        return solve(0,s.size(),s,k,'1');
     }
 };
