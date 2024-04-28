bf9079fe6bba8bd719316fa0a5bd7df8c6e90474
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-04-29 03:33:19 +0530

    Time: 202 ms (66.99%), Space: 99.8 MB (50.49%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
@@ -1,37 +1,41 @@
 class Solution {
+    vector<int>res;
+    vector<int>count;
+    long long root_res=0;
+    int dfs(vector<int>adj[],int root,int prev,int dist){
+        //res[root]+=dist;
+        for(auto itr:adj[root]){
+            if(itr==prev)continue;
+            count[root]+=dfs(adj,itr,root,dist+1);
+            res[root]+=res[itr]+count[itr];
+        }
+        return count[root];
+        
+    }
+    void dfs2(vector<int>adj[],int root,int pre,int n){
+        for(auto itr:adj[root]){
+            if(itr!=pre){
+            res[itr]=res[root]+n-count[itr]-count[itr];
+                dfs2(adj,itr,root,n);
+            }
+        }
+    }
 public:
-    vector<unordered_set<int>> tree;
-    vector<int> res, count;
-
-    vector<int> sumOfDistancesInTree(int N, vector<vector<int>>& edges) {
-        tree.resize(N);
-        res.assign(N, 0);
-        count.assign(N, 1);
-        for (auto e : edges) {
-            tree[e[0]].insert(e[1]);
-            tree[e[1]].insert(e[0]);
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+        vector<int>adj[n];
+        for(auto itr:edges){
+            adj[itr[0]].push_back(itr[1]);
+            adj[itr[1]].push_back(itr[0]);
         }
-        dfs(0, -1);
-        dfs2(0, -1);
+        res.resize(n,0);
+        count.resize(n,1);
+        dfs(adj,0,-1,0);
+        // for(auto itr:count){
+        //     cout<<itr<<" ";
+        // }
+        // cout<<root_res<<endl;
+        dfs2(adj,0,-1,n);
         return res;
-
-    }
-
-    void dfs(int root, int pre) {
-        for (auto i : tree[root]) {
-            if (i == pre) continue;
-            dfs(i, root);
-            count[root] += count[i];
-            res[root] += res[i] + count[i];
-        }
-    }
-
-    void dfs2(int root, int pre) {
-        for (auto i : tree[root]) {
-            if (i == pre) continue;
-            res[i] = res[root] - count[i] + count.size() - count[i];
-            dfs2(i, root);
-        }
+        
     }
 };
-    
