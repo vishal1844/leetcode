da8011b2e3fffda46d911f91a9553fd8ce1090c8
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-05-26 19:19:49 +0530

    Time: 116 ms (20.00%), Space: 28.4 MB (40.00%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.cpp
+++ 3163-string-compression-iii/3163-string-compression-iii.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     string compressedString(string word) {
-      std::stack<char> st;
-    std::string s;
-    
-    for (char itr : word) {
-        if (!st.empty() && st.top() != itr) {
-            s += (st.size() + '0'); // Convert the size to a character
-            s += st.top();
-            while (!st.empty()) st.pop();
+        stack<char>st;
+        int i,j,k,l,m,t=0;
+        string s;
+        for(auto itr:word){
+            if(st.empty()){
+                st.push(itr);
+                continue;
+            }
+            if(st.top()!=itr){
+                s+=st.size()+'0';
+                s+=st.top();
+                while(!st.empty())st.pop();
+            }
+            if(st.size()==9){
+                s+='9';
+                s+=st.top();
+                while(!st.empty())st.pop();
+            }
+            st.push(itr);
         }
-        if (st.size() == 9) {
-            s += '9';
-            s += st.top();
-            while (!st.empty()) st.pop();
+        if(!st.empty()){
+            s+=st.size()+'0';
+            s+=st.top();
         }
-        st.push(itr);
-    }
-    
-    if (!st.empty()) {
-        s += (st.size() + '0'); // Convert the size to a character
-        s += st.top();
-    }
-    
-    return s;
+        return s;
     }
 };
