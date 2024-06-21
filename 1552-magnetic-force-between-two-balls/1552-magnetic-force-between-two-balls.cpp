e5dec8bc09e5df84b0d9f8b7126ce4ba2106edd0
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-06-21 20:45:25 +0530

    Time: 107 ms (95.80%), Space: 61.5 MB (36.17%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,23 +1,37 @@
+#include <vector>
+#include <algorithm>
+
 class Solution {
-    bool valid(vector<int> &A, int M, int m) {
-        int prev = 0;
-        for (int i = 1, j = 1; i < m; ++i) {
-            while (j < A.size() && A[j] < A[prev] + M) ++j;
-            if (j >= A.size()) return false;
-            prev = j;
+    bool solve(std::vector<int>& position, int m, int mid) {
+        int count = 1; // Placing the first item
+        int last_position = position[0]; // The position of the last placed item
+
+        for (int i = 1; i < position.size(); i++) {
+            if (position[i] - last_position >= mid) {
+                count++;
+                last_position = position[i];
+            }
+            if (count >= m) return true;
         }
-        return true;
+        return false;
     }
+
 public:
-    int maxDistance(vector<int>& A, int m) {
-        sort(begin(A), end(A));
-        if (m == 2) return A.back() - A[0];
-        int L = 1, R = A.back() - A[0];
-        while (L <= R) {
-            int M = (L + R) / 2;
-            if (valid(A, M, m)) L = M + 1;
-            else R = M - 1;
+    int maxDistance(std::vector<int>& position, int m) {
+        std::sort(position.begin(), position.end());
+        int left = 1; // Minimum possible distance
+        int right = position.back() - position.front(); // Maximum possible distance
+        int result = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (solve(position, m, mid)) {
+                result = mid;
+                left = mid + 1; // Try for a larger minimum distance
+            } else {
+                right = mid - 1; // Try for a smaller minimum distance
+            }
         }
-        return R;
+        return result;
     }
 };
