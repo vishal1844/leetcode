4924c6288a1657c2e02d51a11901a5712d5f0177
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-07-02 17:50:04 +0530

    Time: 372 ms (19.61%), Space: 145.1 MB (22.84%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,39 +1,32 @@
 class Solution {
-    vector<set<int>> v;
-    vector<int> visit;
-    
-    void dfs(int i, vector<int> adj[]) {
-        visit[i] = 1;
-        for (auto itr : adj[i]) {
-            if (!visit[itr]) {
-                dfs(itr, adj);
+    vector<set<int>>v;
+    vector<int>visit;
+    void dfs(int i,vector<int>adj[]){
+        for(auto itr:adj[i]){
+            if(!visit[itr]){
+                dfs(itr,adj);
             }
-            v[i].insert(itr);
-            v[i].insert(v[itr].begin(), v[itr].end());
-        }
+                v[i].insert(itr);
+                v[i].insert(v[itr].begin(),v[itr].end());
+            }
+       visit[i]=1;
     }
-    
 public:
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
         v.resize(n);
-        visit.resize(n, 0);
-        vector<vector<int>> ans(n);
-        vector<int> adj[n];
-        
-        for (auto itr : edges) {
+        visit.resize(n,0);
+        vector<vector<int>>ans(n);
+        vector<int>adj[n];
+        for(auto itr:edges){
             adj[itr[1]].push_back(itr[0]);
         }
-        
-        for (int i = 0; i < n; i++) {
-            if (!visit[i]) {
-                dfs(i, adj);
-            }
+        for(int i=0;i<n;i++){
+            if(!visit[i])
+            dfs(i,adj);
         }
-        
-        for (int i = 0; i < n; i++) {
-            ans[i] = vector<int>(v[i].begin(), v[i].end());
+        for(int i=0;i<n;i++){
+            ans[i]=vector<int>(v[i].begin(),v[i].end());
         }
-        
         return ans;
     }
 };
