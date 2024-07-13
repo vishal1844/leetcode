74f87082f6d74149ca51ece4a96a55ec2f9ecafd
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-07-14 04:21:04 +0530

    Time: 495 ms (12.96%), Space: 304.9 MB (7.78%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.cpp
+++ 2751-robot-collisions/2751-robot-collisions.cpp
@@ -1,62 +1,50 @@
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <stack>
-
-using namespace std;
-
 class Solution {
-    static bool comp(vector<int>& a, vector<int>& b) {
-        return a[3] < b[3];
+    static bool comp(vector<int>&a,vector<int>&b){
+        return a[3]<b[3];
     }
 public:
-    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
-        vector<vector<int>> robots;
-        int n = positions.size();
-
-        for (int i = 0; i < n; ++i) {
-            robots.push_back({positions[i], healths[i], directions[i], i});
+    vector<int> survivedRobotsHealths(vector<int>& p, vector<int>& h, string d) {
+        vector<vector<int>>v;
+        int i,j,k,l,m,t=0;
+        for(i=0;i<p.size();i++){
+            v.push_back({p[i],h[i],d[i],i});
         }
-
-        sort(robots.begin(), robots.end());
-
-        vector<vector<int>> stack;
-
-        for (auto& robot : robots) {
-            int pos = robot[0];
-            int health = robot[1];
-            char dir = robot[2];
-            int index = robot[3];
-
-            if (stack.empty() || dir == 'R' || stack.back()[2] == 'L') {
-                stack.push_back(robot);
-            } else {
-                bool flag = true;
-                while (!stack.empty() && flag && stack.back()[2] == 'R') {
-                    if (stack.back()[1] == health) {
-                        flag = false;
-                        stack.pop_back();
-                    } else if (stack.back()[1] > health) {
-                        stack.back()[1]--;
-                        flag = false;
-                    } else {
+        sort(v.begin(),v.end());
+        vector<vector<int>>st;
+        for(auto itr:v){
+            int pos=itr[0];
+            int health=itr[1];
+            int dir=itr[2];
+            int ind=itr[3];
+            if(st.empty()||dir=='R'){
+                st.push_back(itr);
+            }
+            else{
+                int flag=1;
+                while(!st.empty()&&flag&&st.back()[2]=='R'){
+                    if(st.back()[1]==health){
+                        flag=0;
+                        st.pop_back();
+                    }
+                    else if(st.back()[1]>health){
+                        st.back()[1]--;
+                        flag=0;
+                    }
+                    else{
                         health--;
-                        stack.pop_back();
+                        st.pop_back();
                     }
                 }
-                if (flag) {
-                    stack.push_back({pos, health, dir, index});
+                if(flag==1){
+                    st.push_back({pos,health,dir,ind});
                 }
             }
         }
-
-        sort(stack.begin(), stack.end(), comp);
-
-        vector<int> result;
-        for (auto& robot : stack) {
-            result.push_back(robot[1]);
+        sort(st.begin(),st.end(),comp);
+        vector<int>ans;
+        for(auto itr:st){
+            ans.push_back(itr[1]);
         }
-
-        return result;
+        return ans;
     }
 };
