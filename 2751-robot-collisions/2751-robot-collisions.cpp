94db4ddadfd7d68dab6bb1889b443d043731754a
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-07-14 04:17:07 +0530

    Time: 414 ms (18.15%), Space: 278.2 MB (13.33%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.cpp
+++ 2751-robot-collisions/2751-robot-collisions.cpp
@@ -1,8 +1,18 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
 class Solution {
+    static bool comp(vector<int>& a, vector<int>& b) {
+        return a[3] < b[3];
+    }
 public:
     vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
-        int n = positions.size();
         vector<vector<int>> robots;
+        int n = positions.size();
 
         for (int i = 0; i < n; ++i) {
             robots.push_back({positions[i], healths[i], directions[i], i});
@@ -13,38 +23,36 @@ public:
         vector<vector<int>> stack;
 
         for (auto& robot : robots) {
-            if (robot[2] == 'R' || stack.empty() || stack.back()[2] == 'L') {
-                stack.push_back(robot);
-                continue;
-            }
+            int pos = robot[0];
+            int health = robot[1];
+            char dir = robot[2];
+            int index = robot[3];
 
-            if (robot[2] == 'L') {
-                bool add = true;
-                while (!stack.empty() && stack.back()[2] == 'R' && add) {
-                    int last_health = stack.back()[1];
-                    if (robot[1] > last_health) {
+            if (stack.empty() || dir == 'R' || stack.back()[2] == 'L') {
+                stack.push_back(robot);
+            } else {
+                bool flag = true;
+                while (!stack.empty() && flag && stack.back()[2] == 'R') {
+                    if (stack.back()[1] == health) {
+                        flag = false;
                         stack.pop_back();
-                        robot[1] -= 1;
-                    } else if (robot[1] < last_health) {
-                        stack.back()[1] -= 1;
-                        add = false;
+                    } else if (stack.back()[1] > health) {
+                        stack.back()[1]--;
+                        flag = false;
                     } else {
+                        health--;
                         stack.pop_back();
-                        add = false;
                     }
                 }
-
-                if (add) {
-                    stack.push_back(robot);
+                if (flag) {
+                    stack.push_back({pos, health, dir, index});
                 }
             }
         }
 
-        vector<int> result;
-        sort(stack.begin(), stack.end(), [](vector<int>& a, vector<int>& b) {
-            return a[3] < b[3];
-        });
+        sort(stack.begin(), stack.end(), comp);
 
+        vector<int> result;
         for (auto& robot : stack) {
             result.push_back(robot[1]);
         }
