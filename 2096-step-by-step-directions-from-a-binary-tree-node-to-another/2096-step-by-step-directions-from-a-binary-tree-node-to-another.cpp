fc4c5f2bea6b486269cdf4d4bcc305f3b77cff34
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-07-17 04:02:38 +0530

    Time: 152 ms (66.11%), Space: 119.6 MB (37.71%) - LeetHub

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    string getDirections(TreeNode* root, int startValue, int destValue) {
+        string startPath, destPath;
+        findPath(root, startValue, startPath);
+        findPath(root, destValue, destPath);
+
+        string directions;
+        int commonPathLength = 0;
+        while (commonPathLength < startPath.length() &&
+               commonPathLength < destPath.length() &&
+               startPath[commonPathLength] == destPath[commonPathLength]) {
+            commonPathLength++;
+        }
+        for (int i = 0; i < startPath.length() - commonPathLength; i++) {
+            directions += "U";
+        }
+        for (int i = commonPathLength; i < destPath.length(); i++) {
+            directions += destPath[i];
+        }
+
+        return directions;
+    }
+
+private:
+    bool findPath(TreeNode* node, int target, string& path) {
+        if (node == nullptr) {
+            return false;
+        }
+        if (node->val == target) {
+            return true;
+        }
+        path += "L";
+        if (findPath(node->left, target, path)) {
+            return true;
+        }
+        path.pop_back(); 
+        path += "R";
+        if (findPath(node->right, target, path)) {
+            return true;
+        }
+        path.pop_back();
+
+        return false;
+    }
+};
