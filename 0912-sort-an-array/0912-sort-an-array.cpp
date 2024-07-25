88002d05f074f3200eb9b6665f40ceb49c7db4fe
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-07-25 16:33:43 +0530

    Time: 90 ms (85.00%), Space: 70.5 MB (68.95%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,52 +1,7 @@
 class Solution {
-    void mergesort(int l,vector<int>&nums,vector<int>&v,int mid,int h){
-        int i=l;
-        int j=l;
-        int k=mid+1; 
-        while(i<=mid&&k<=h){
-            if(nums[i]<nums[k]){
-                //v.push_back(nums[i]);
-                v[j++]=nums[i];
-                i++;
-            }
-            else{
-                //v.push_back(nums[k]);
-                v[j++]=nums[k];
-                k++;
-            }
-        }
-        while(i<=mid){
-            //v.push_back(nums[i]);
-            v[j++]=nums[i];
-            i++;
-        }
-        while(k<=h){
-           // v.push_back(nums[k]);
-            v[j++]=nums[k];
-            k++;
-        }
-        // for(i=l;i<v.;i++){
-            // cout<<v[i]<<" ";
-        // }
-        // cout<<endl;
-        for(int i=l;i<=h;i++){
-           nums[i]=v[i];
-            }
-        // return ;
-    }
-    void merge(int l,vector<int>&nums,vector<int>&v,int h){
-        if(l<h){
-            int mid=(l+h)/2;
-            merge(l,nums,v,mid);
-            merge(mid+1,nums,v,h);
-            mergesort(l,nums,v,mid,h);
-        }
-    }
 public:
     vector<int> sortArray(vector<int>& nums) {
-        vector<int>v(nums.size(),0);
-        int h=nums.size()-1;
-        merge(0,nums,v,h);
-        return v;
+        sort(nums.begin(),nums.end());
+        return nums;
     }
 };
