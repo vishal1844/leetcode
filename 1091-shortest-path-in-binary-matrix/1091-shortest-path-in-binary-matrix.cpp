b7db55c67bc5d9439fb83667e9ce5f6bf88d1be3
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-07-26 04:07:43 +0530

    Time: 38 ms (89.69%), Space: 24.7 MB (58.13%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,30 +1,52 @@
 class Solution {
-public:
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        queue<pair<int, int>> queue;
-    queue.push(make_pair(0,0));
-        int row=grid.size();
-        int col=grid[0].size();
-        if(grid[0][0]==1){
-            return -1;
+    vector<vector<int>> directions = {{1, 1}, {0, 1}, {1, 0}, {0, -1}, {-1, 0}, {-1, -1}, {1, -1}, {-1, 1}};
+    int maxi = 0;
+
+    int solve(int i, int j, int n, int m, vector<vector<int>>& vis, vector<vector<int>>& grid) {
+        if (i == n && j == m) {
+            maxi = 1;
+            return 0;
         }
-    vector<vector<int>> directions = {{1,1}, {0,1},{1,0},{0,-1},{-1,0},{-1, -1},{1, -1},{-1, 1}};
-    grid[0][0] = 1;
-    while(!queue.empty()){
-        auto curr = queue.front();
-        int x = curr.first, y = curr.second;
-        if( x == row -1 && y == col -1) return grid[x][y];
-        
-        for(auto direction : directions){
-            int nx = x + direction[0];
-            int ny = y + direction[1];
-            if(nx >= 0 && nx < row && ny >= 0 && ny < col && grid[nx][ny] == 0){
-                queue.push(make_pair(nx,ny));
-                grid[nx][ny] = grid[x][y] + 1;
+        vis[i][j] = 1;
+        int k = INT_MAX-1;
+        for (auto& itr : directions) {
+            int newi = itr[0] + i;
+            int newj = itr[1] + j;
+            if (newi <= n && newj <= m && newi >= 0 && newj >= 0 && grid[newi][newj] == 0 && !vis[newi][newj]) {
+                k = min(k, 1 + solve(newi, newj, n, m, vis, grid));
             }
         }
-        queue.pop();
+        vis[i][j] = 0;  // Backtrack
+        return k;
     }
-    return -1;
-}
+
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        queue<pair<int,int>>q;
+        if(grid[0][0]==1)return -1;
+        if(grid[n-1][m-1]==1)return -1;
+        q.push({0,0});
+        grid[0][0]=1;
+        while(!q.empty()){
+            auto itr=q.front();
+            q.pop();
+            int i=itr.first;
+            int j=itr.second;
+            if(i==n-1&&j==m-1)return grid[i][j];
+             for (auto& itr : directions) {
+            int newi = itr[0] + i;
+            int newj = itr[1] + j;
+            if (newi <n && newj < m && newi >= 0 && newj >= 0 && grid[newi][newj] == 0) {
+                q.push({newi,newj});
+                grid[newi][newj]=grid[i][j]+1;
+            }
+        }
+           // return -1;
+            
+        }
+        return -1;
+    }
 };
