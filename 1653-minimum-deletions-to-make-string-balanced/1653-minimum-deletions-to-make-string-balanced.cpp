71b9e2c1071795d7297b87be17f50e6d767824d5
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-07-31 00:35:32 +0530

    Time: 75 ms (91.23%), Space: 23.7 MB (73.37%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -3,13 +3,15 @@ public:
     int minimumDeletions(string s) {
         int i,j,k,l=0,m=0,t=0;
         for(i=0;i<s.size();i++){
-            if(s[i]=='a')m++;
-        }
-        t=s.size();
-        for(i=0;i<s.size();i++){
-            if(s[i]=='a')m--;
-            t=min(t,m+l);
-            if(s[i]=='b')l++;
+            if(s[i]=='a'){
+                if(m){
+                    m--;
+                    t++;
+                }
+            }
+            else{
+                m++;
+            }
         }
         return t;
     }
