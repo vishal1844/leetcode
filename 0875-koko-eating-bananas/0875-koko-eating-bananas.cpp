bd7e2306d23cbbb6e6b4783b90a5cf71e6cc4666
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-08-05 00:28:14 +0530

    Time: 35 ms (42.86%), Space: 21.7 MB (39.87%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,16 +1,36 @@
 class Solution {
-public:
-    int minEatingSpeed(vector<int>& piles, int H) {
-        int l = 1, r = 1000000000;
-        while (l < r) {
-            int m = (l + r) / 2, total = 0;
-            for (int p : piles)
-                total += (p + m - 1) / m;
-            if (total > H)
-                l = m + 1;
-            else
-                r = m;
+    bool eatAllBananas(vector<int>&piles,int h,int mid){
+        for(int i=0;i<piles.size();i++){
+            if(h<=0)return false;
+            if(piles[i]<=mid){
+                h--;
+            }
+            else{
+                h-=(piles[i]/mid);
+                if(piles[i]%mid){
+                    h--;
+                }
+            }
         }
-        return l;
+        if(h>=0)return true;
+        return false;
+    }
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n=piles.size();
+        int low=1;
+        int high=*max_element(piles.begin(),piles.end());
+        int ans=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(eatAllBananas(piles,h,mid)){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
     }
 };
