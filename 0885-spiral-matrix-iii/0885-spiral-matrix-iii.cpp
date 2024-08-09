034a676e8b16e1d2e31513efc1ab49d029bcd1e6
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-08-10 01:15:43 +0530

    Time: 7 ms (85.40%), Space: 13.6 MB (82.07%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.cpp
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.cpp
@@ -1,31 +1,25 @@
 class Solution {
+    int a[4]={0,1,0,-1};
+    int b[4]={1,0,-1,0};
 public:
-    vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart,
-                                        int cStart) {
-        // Store all possible directions in an array.
-        vector<vector<int>> dir{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        vector<vector<int>> traversed;
-
-        // Initial step size is 1, value of d represents the current direction.
-        for (int step = 1, direction = 0; traversed.size() < rows * cols;) {
-            // direction = 0 -> East, direction = 1 -> South
-            // direction = 2 -> West, direction = 3 -> North
-            for (int i = 0; i < 2; ++i) {
-                for (int j = 0; j < step; ++j) {
-                    // Validate the current position
-                    if (rStart >= 0 && rStart < rows && cStart >= 0 &&
-                        cStart < cols) {
-                        traversed.push_back({rStart, cStart});
-                    }
-                    // Make changes to the current position.
-                    rStart += dir[direction][0];
-                    cStart += dir[direction][1];
-                }
-
-                direction = (direction + 1) % 4;
+    vector<vector<int>> spiralMatrixIII(int rows, int cols, int rs, int cs) {
+        vector<vector<int>>ans;
+        ans.push_back({rs,cs});
+        int steps=0;
+        int dir=0;
+        while(ans.size()<(rows*cols)){
+            if(dir==0||dir==2){
+                steps++;
             }
-            ++step;
+            for(int i=0;i<steps;i++){
+                rs+=a[dir];
+                cs+=b[dir];
+                if(rs>=0&&rs<rows&&cs>=0&&cs<cols){
+                    ans.push_back({rs,cs});
+                }
+            }
+            dir=(dir+1)%4;
         }
-        return traversed;
+        return ans;
     }
 };
