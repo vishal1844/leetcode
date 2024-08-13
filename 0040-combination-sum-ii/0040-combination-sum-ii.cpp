14cb80d09e97994139e1190dc13195958747bfd0
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-08-14 01:06:41 +0530

    Time: 3 ms (77.80%), Space: 13 MB (34.48%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,42 +1,28 @@
 class Solution {
-    set<vector<int>>st;
-    void solve(int ind,int n,vector<int>&cand,int target,vector<int>v,vector<vector<int>>&ans){
-        if(ind==n){
-            if(target==0){
-                st.insert(v);
-                return;
-            }
-            if(target>0){
-                return;
-            }
-        }
+    vector<vector<int>>ans;
+    vector<int>v;
+    void solve(int i,int n,vector<int>&a,int target){
         if(target==0){
-            st.insert(v);
+            ans.push_back(v);
             return;
         }
-        if(target<0){
-            return;
-        }
-        for(int i=ind;i<n;i++){
-            if(i>ind&&cand[i]==cand[i-1])continue;
-            if(cand[i]<=target){
-                v.push_back(cand[i]);
-                solve(i+1,n,cand,target-cand[i],v,ans);
+        if(i>=n)return;
+        if(target<0)return;
+        for(int k=i;k<n;k++){
+            if(k>i&&a[k]==a[k-1])continue;
+            if(target>=a[k]){
+                v.push_back(a[k]);
+                solve(k+1,n,a,target-a[k]);
                 v.pop_back();
             }
         }
+        return;
     }
 public:
-    vector<vector<int>> combinationSum2(vector<int>& cand, int target) {
-        vector<vector<int>>ans;
-        vector<int>v;
-        sort(cand.begin(),cand.end());
-        int t=accumulate(cand.begin(),cand.end(),0);
-        if(t<target)return ans;
-        solve(0,cand.size(),cand,target,v,ans);
-        for(auto itr:st){
-            ans.push_back(itr);
-        }
+    vector<vector<int>> combinationSum2(vector<int>& c, int target) {
+        sort(c.begin(),c.end());
+        int i,j,k,l,m,t=0;
+        solve(0,c.size(),c,target);
         return ans;
     }
 };
