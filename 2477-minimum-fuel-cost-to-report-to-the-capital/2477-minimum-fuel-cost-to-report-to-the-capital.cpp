d0f15f76cfe091cc729a8d8def003a80df7c7e2b
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-08-18 18:53:21 +0530

    Time: 423 ms (61.19%), Space: 184.1 MB (58.10%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -1,33 +1,31 @@
 class Solution {
-public:
-    long long fuel;
-
-    long long dfs(int node, int parent, vector<vector<int>>& adj, int& seats) {
-        // The node itself has one representative.
-        int representatives = 1;
-        for (auto& child : adj[node]) {
-            if (child != parent) {
-                // Add count of representatives in each child subtree to the parent subtree.
-                representatives += dfs(child, node, adj, seats);
-            }
+    vector<int>vis;
+    long long ans=0;
+    int dfs(int i,int n,vector<int>adj[n],int seats){
+       vis[i]=1;
+        int t=1;
+        for(auto itr:adj[i]){
+            if(vis[itr])continue;
+            t+=dfs(itr,n,adj,seats);
         }
-
-        if (node != 0) {
-            // Count the fuel it takes to move to the parent node.
-            // Root node does not have any parent so we ignore it.
-            fuel += ceil((double)representatives / seats);
+        if(i!=0){
+            ans+=ceil(t/(seats*1.0));
         }
-        return representatives;
+        return t;
     }
-
+public:
     long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
-        int n = roads.size() + 1;
-        vector<vector<int>> adj(n);
-        for (auto& road : roads) {
-            adj[road[0]].push_back(road[1]);
-            adj[road[1]].push_back(road[0]);
+        long long t=0;
+        int n=roads.size();
+        int i,j,k,l,m;
+        vis.resize(n+1,0);
+        vector<int>adj[n+1];
+        for(auto itr:roads){
+            adj[itr[0]].push_back(itr[1]);
+            adj[itr[1]].push_back(itr[0]);
         }
-        dfs(0, -1, adj, seats);
-        return fuel;
+        dfs(0,n+1,adj,seats);
+        return ans;
+        
     }
 };
