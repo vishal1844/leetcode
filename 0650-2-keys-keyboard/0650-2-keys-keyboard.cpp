6da46499d519572124f72df4aa9c7e8d3b31d50d
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-08-20 00:48:07 +0530

    Time: 8 ms (48.73%), Space: 8.2 MB (30.87%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     int minSteps(int n) {
-        int d=2;
-        int i,j,k,l,t=0;
-        while(n>1){
-            while(n%d==0){
-                t+=d;
-                n/=d;
+        if(n==1)return 0;
+        if(n==2)return 2;
+        int i,j,k,l,m;
+        vector<int>a(n+1,0);
+        a[2]=2;
+        for(i=3;i<=n;i++){
+            k=i/2;
+            while(k>=1){
+                if(i%k==0){
+                    int steps=a[k];
+                    int paste=i/k-1;
+                    int copy=1;
+                    a[i]=steps+copy+paste;
+                    break;
+                }
+                else k--;
             }
-            d++;
         }
-        return t;
+        return a[n];
     }
 };
