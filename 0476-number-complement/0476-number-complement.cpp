eeb63323e7137b340c2b2d0ad24d7bee25a0fd24
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-08-22 19:13:33 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (33.08%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int findComplement(int num) {
         int i,j,l,m,t=0;
-        long long k=1;
+        long long k=0;
         while(num!=0){
             if(num%2==0)
-              t+=k;  
-            k*=2;
+              t+=1<<k;  
+            k++;
             num/=2;
         }
         return t;
