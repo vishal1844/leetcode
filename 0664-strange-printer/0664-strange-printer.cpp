f08b33a63aa0df1036be64d459ceb67a6eadf39b
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-08-24 00:50:06 +0530

    Time: 15 ms (71.67%), Space: 8.6 MB (94.65%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.cpp
+++ 0664-strange-printer/0664-strange-printer.cpp
@@ -1,22 +1,31 @@
 class Solution {
-public:
-    int strangePrinter(string s) {
-        int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
-        
-        for (int i = n - 1; i >= 0; --i) {
-            dp[i][i] = 1;
-            for (int j = i + 1; j < n; ++j) {
-                if (s[i] == s[j]) {
-                    dp[i][j] = dp[i][j - 1];
-                } else {
-                    for (int k = i; k < j; ++k) {
-                        dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j]);
-                    }
-                }
+    int dp[101][101];
+    int repeat(int i,int n,string&s){
+        int j,k,l;
+        while(i<n&&(s[i]==s[i-1])){
+            i++;
+        }
+        return i;
+    }
+    int solve(int i,int n,string &s){
+        if(i>=n)return 0;
+        int k=repeat(i+1,n,s);
+        if(k>=n)return 1;
+        if(dp[i][n]!=-1)return dp[i][n];
+        int basic=1+solve(k,n,s);
+        int lalach=INT_MAX;
+        for(k=i+1;k<n;k++){
+            if(s[k]==s[i]){
+                lalach=min(lalach,solve(i+1,k,s)+solve(k,n,s));
             }
         }
+        return dp[i][n]=min(lalach,basic);
         
-        return dp[0][n - 1];
+    }
+public:
+    int strangePrinter(string s) {
+        int n=s.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(0,n,s);
     }
 };
