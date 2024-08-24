192eacc531835ad1a563e21a32bbd9bd655116d1
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-08-24 15:19:04 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (34.93%) - LeetHub

--- 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.cpp
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.cpp
@@ -1,44 +1,67 @@
+#include <string>
+
 class Solution {
 public:
-    string fractionAddition(string expression) {
-        int num = 0;
-        int denom = 1;
-
-        // separate expression into signed numbers
-        vector<string> nums;
-        int i = 0;
-        if (expression[0] != '-') expression = '+' + expression;
-        while (i < expression.size()) {
-            int j = i + 1;
-            while (j < expression.size() && expression[j] != '+' &&
-                   expression[j] != '-') {
-                j++;
-            }
-            nums.push_back(expression.substr(i, j - i));
-            i = j;
+    // Helper function to calculate GCD (Greatest Common Divisor)
+    long long gcd(long long a, long long b) {
+        while (b != 0) {
+            long long temp = b;
+            b = a % b;
+            a = temp;
         }
-
-        for (int i = 0; i < nums.size(); ++i) {
-            size_t pos = nums[i].find('/');
-            int currNum = stoi(nums[i].substr(1, pos - 1));
-            if (nums[i][0] == '-') currNum = -currNum;
-            int currDenom = stoi(nums[i].substr(pos + 1));
-
-            num = num * currDenom + currNum * denom;
-            denom = denom * currDenom;
-
-            int gcd = abs(FindGCD(num, denom));
-
-            num /= gcd;
-            denom /= gcd;
-        }
-
-        return to_string(num) + "/" + to_string(denom);
+        return a;
     }
 
-private:
-    int FindGCD(int a, int b) {
-        if (a == 0) return b;
-        return FindGCD(b % a, a);
+    std::string fractionAddition(std::string e) {
+        long long n = 0;  // Numerator of the result
+        long long d = 1;  // Denominator of the result
+        long long curn = 1;  // Current fraction numerator
+        long long curd = 1;  // Current fraction denominator
+        char exp = '+';  // Current sign
+
+        // Prepend '+' if the expression doesn't start with a sign
+        if (e[0] != '-') e = '+' + e;
+
+        int i = 0;
+        while (i < e.size()) {
+            exp = e[i];  // Current operation ('+' or '-')
+            i++;
+
+            // Read the numerator
+            curn = 0;
+            while (i < e.size() && e[i] != '/') {
+                curn = curn * 10 + (e[i] - '0');
+                i++;
+            }
+            i++;  // Skip '/'
+
+            // Read the denominator
+            curd = 0;
+            while (i < e.size() && e[i] != '+' && e[i] != '-') {
+                curd = curd * 10 + (e[i] - '0');
+                i++;
+            }
+
+            // Apply the sign to the numerator
+            if (exp == '-') curn = -curn;
+
+            // Perform the fraction addition
+            n = n * curd + d * curn;
+            d = d * curd;
+
+            // Reduce the fraction by its GCD
+            long long t = gcd(abs(n), d);  // Use absolute value of n for gcd calculation
+            n /= t;
+            d /= t;
+
+            // Ensure the denominator is positive
+            if (d < 0) {
+                n = -n;
+                d = -d;
+            }
+        }
+
+        // Return the resulting fraction as a string
+        return std::to_string(n) + '/' + std::to_string(d);
     }
 };
