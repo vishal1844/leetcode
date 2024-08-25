a5f2d1f438ee9183beafa7d0c2368ef5c406549b
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-08-26 02:43:43 +0530

    Time: 120 ms (100.00%), Space: 57.2 MB (100.00%) - LeetHub

---
+++ 3266-final-array-state-after-k-multiplication-operations-ii/3266-final-array-state-after-k-multiplication-operations-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    long long modpow(long long a, long long b, long long m) {long long  res = 1; while (b > 0) {if (b & 1)res = (res * a) % m; a = (a * a) % m; b = b >> 1;} return res;}
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int mul) {
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+        int mod = 1e9 + 7;
+        unordered_map<int,int>mp;
+        int maxi=*max_element(nums.begin(),nums.end());
+        for (int i = 0; i < nums.size(); i++) {
+            pq.push({nums[i], i});
+        }
+        if(mul==1)return nums;
+        while (1) {
+            auto itr = pq.top();
+            long long p=(itr.first*mul);
+            if(p>maxi||mp.size()==nums.size())
+                break;
+            pq.pop();
+            pq.push({(itr.first * mul)%mod, itr.second});
+            k--;
+            mp[itr.second]++;
+            if(k<=0)break;
+        }
+        int n=nums.size();
+        long long pow=modpow(mul,k/n,mod);
+        while (!pq.empty()) {
+            auto itr = pq.top();
+            pq.pop();
+            nums[itr.second] = 1LL*((1LL*itr.first%mod)*(pow%mod))%mod;
+            if(k%n!=0){
+                k--;
+                nums[itr.second]=1LL*((1LL*nums[itr.second]%mod)*(mul%mod))%mod;            
+            }
+        }
+        return nums;
+    }
+};
