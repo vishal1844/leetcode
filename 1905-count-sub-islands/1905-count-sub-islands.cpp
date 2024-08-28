abf6894d3654bad46800ac3d87f698c93bc568a5
Vishal Jindal <104387858+vishal1844@users.noreply.github.com>
2024-08-28 15:35:45 +0530

    Time: 215 ms (61.69%), Space: 95.9 MB (57.18%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,47 +1,39 @@
 class Solution {
-    int a[4] = {1, 0, -1, 0};
-    int b[4] = {0, 1, 0, -1};
-    bool check = true;
-
-    void solve(int i, int j, vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        grid1[i][j] = 0; // Mark cell as visited in grid1
-        grid2[i][j] = 0; // Mark cell as visited in grid2
-
-        for (int k = 0; k < 4; k++) {
-            int newi = i + a[k];
-            int newj = j + b[k];
-
-            // Continue DFS if new cell is valid and unvisited
-            if (newi >= 0 && newi < grid1.size() && newj >= 0 && newj < grid1[0].size()) {
-                if (grid2[newi][newj] == 1) {
-                    if (grid1[newi][newj] == 0) {
-                        check = false; // If grid1 has no land but grid2 does, it's not a valid sub-island
-                    } else {
-                        solve(newi, newj, grid1, grid2); // Continue DFS
-                    }
-                }
+    int a[4]={1,0,-1,0};
+    int b[4]={0,1,0,-1};
+    bool check=1;
+    void solve(int i,int j,vector<vector<int>>&grid1,vector<vector<int>>&grid2){
+        grid1[i][j]=0;
+        grid2[i][j]=0;
+        for(int k=0;k<4;k++){
+            int newi=i+a[k];
+            int newj=j+b[k];
+            if(newi>=0&&newi<grid1.size()&&newj>=0&&newj<grid1[0].size()&&grid2[newi][newj]==1&&grid1[newi][newj]==1){
+                solve(newi,newj,grid1,grid2);
+            }
+            else if(newi>=0&&newi<grid1.size()&&newj>=0&&newj<grid1[0].size()){
+                if(grid1[newi][newj]==0&&grid2[newi][newj]==1)check=0;
             }
         }
+        return;
     }
-
+    
+    
 public:
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        int n = grid1.size();
-        int m = grid2[0].size(); // Corrected dimension calculation
-        int t = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid2[i][j] == 1 && grid1[i][j] == 1) {
-                    check = true;
-                    solve(i, j, grid1, grid2);
-                    if (check) {
+        int n=grid1.size();
+        int m=grid1[0].size();
+        int i,j,k,l,t=0;
+        for(i=0;i<n;i++){
+            for(j=0;j<m;j++){
+                if(grid2[i][j]==1 && grid1[i][j]==1){
+                    check=1;
+                    solve(i,j,grid1,grid2);
+                        if(check)
                         t++;
-                    }
                 }
             }
-        }
-
+        }           
         return t; 
     }
 };
